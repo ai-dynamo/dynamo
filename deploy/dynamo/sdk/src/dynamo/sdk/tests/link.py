565f66da6c5e672b46cd8aa77c68a8a452708eb8
Biswa Panda <biswa.panda@gmail.com>
2025-03-14 13:14:02 -0700

    fix: remove brainstroming ideas from PR

--- deploy/dynamo/sdk/src/dynamo/sdk/tests/link.py
+++ deploy/dynamo/sdk/src/dynamo/sdk/tests/link.py
@@ -29,49 +29,6 @@ from dynamo.sdk.tests.pipeline import Backend, Frontend, Middle
 print()
 Frontend.link(Middle).build()
 
-"""
----------------
-class FrontEnd:
-    decode_worker = depends(DecodeWorker)
-    preproc = depends(Preproc)              # optional
-
-class Processor:
-    kv_router = depends(KvRouter)
-
-class KvRouter:
-    decode_worker = depends(DecodeWorker)
-
-class DecodeWorker:
-    prefill_worker = depends(PrefillWorker) # optional
----------------
-
-# Optional
-Frontend -> Preproc
-
-if cli.enable_prefill:
-    Frontend -> Preproc -> KvRouter
-
-if cli.enable_prefill:
-    DecodeWorker -> PrefillWorker
------------
-
-Monolith
-Frontend.link(DecodeWorker).build()
-
-Kv aware monolith
-Frontend.link(Processor).link(DecodeWorker).build()
-Frontend.link(KvRouter)
-
-Kv off + Disag on
-Frontend.link(DecodeWorker).link(PrefillWorker).build()
-if kv_disabled:
-    Processor.unlink(KvRouter)
-
-Kv on + Disag On
-Frontend.link(Processor)
-
-"""
-
 print("Frontend dependencies", Frontend.dependencies)
 print("Middle dependencies", Middle.dependencies)
 print("Backend dependencies", Backend.dependencies)
