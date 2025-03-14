4c1c873b3eedbcfd50f1c8dc1de682b5b5710de2
ishandhanani <ishandhanani@gmail.com>
2025-03-14 19:37:37 +0000

    precommit

--- deploy/examples/vllm/disaggregated/ex1.py
+++ deploy/examples/vllm/disaggregated/ex1.py
@@ -2,7 +2,6 @@ from disaggregated.frontend import Frontend
 from disaggregated.kv_router import Router
 from disaggregated.processor import Processor
 from disaggregated.worker import VllmWorker
-from disaggregated.prefill_worker import PrefillWorker
 
 # example 2 and 3: kv aware routing + worker
 # kv.yaml
@@ -14,4 +13,4 @@ Frontend.link(Processor).link(Router).link(VllmWorker)
 
 # example 6: disag with kv
 # kv_with_disag.yaml
-#Frontend.link(Processor).link(Router).link(VllmWorker).link(PrefillWorker)
+# Frontend.link(Processor).link(Router).link(VllmWorker).link(PrefillWorker)
