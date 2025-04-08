85d2e95934bee3aa900314659926f4384e521d39
jon-chuang <9093549+jon-chuang@users.noreply.github.com>
2025-04-07 20:41:44 -0700

    add backwards compat

--- deploy/dynamo/sdk/src/dynamo/sdk/tests/config.py
+++ deploy/dynamo/sdk/src/dynamo/sdk/tests/config.py
@@ -49,3 +49,19 @@ true, "router": "random", "tensor-parallel-size": 1, "ServiceArgs": {"workers": 
         for key in ["model", "block-size", "max-model-len"]
         for arg in vllm_worker_args
     )
+
+
+def test_service_config_no_common_config():
+    ServiceConfig._instance = None
+    os.environ[
+        "DYNAMO_SERVICE_CONFIG"
+    ] = """
+{"Frontend": {"served_model_name": "deepseek-ai/DeepSeek-R1-Distill-Llama-8B", "endpoint": "dynamo.Processor.chat/completions", "port": 8000}, "Processor": {"router": "round-robin", "common-configs": ["model", "block-size", "max-model-len"]}, "VllmWorker": {"enforce-eager": true, "max-num-batched-tokens": 16384, "enable-prefix-caching":
+true, "router": "random", "tensor-parallel-size": 1, "ServiceArgs": {"workers": 1}, "model": "deepseek-ai/DeepSeek-R1-Distill-Llama-8B", "block-size": 64, "max-model-len": 16384]}}
+"""
+    service_config = ServiceConfig.get_instance()
+    vllm_worker_args = service_config.as_args("VllmWorker")
+    assert all(
+        any(arg == f"--{key}" for arg in vllm_worker_args)
+        for key in ["model", "block-size", "max-model-len"]
+    )
