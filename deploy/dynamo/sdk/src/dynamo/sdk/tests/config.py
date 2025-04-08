83ae44e7d13d5fd8801b75d31f6f945b1abf8267
jon-chuang <9093549+jon-chuang@users.noreply.github.com>
2025-04-07 19:26:11 -0700

    done

--- deploy/dynamo/sdk/src/dynamo/sdk/tests/config.py
+++ deploy/dynamo/sdk/src/dynamo/sdk/tests/config.py
@@ -26,7 +26,7 @@ def test_service_config_not_subscribed():
         "DYNAMO_SERVICE_CONFIG"
     ] = """
 {"Common": {"model": "deepseek-ai/DeepSeek-R1-Distill-Llama-8B", "block-size": 64, "max-model-len": 16384}, "Frontend": {"served_model_name": "deepseek-ai/DeepSeek-R1-Distill-Llama-8B", "endpoint": "dynamo.Processor.chat/completions", "port": 8000}, "Processor": {"router": "round-robin", "common-configs": ["model", "block-size", "max-model-len"]}, "VllmWorker": {"enforce-eager": true, "max-num-batched-tokens": 16384, "enable-prefix-caching":
-true, "router": "random", "tensor-parallel-size": 1, "ServiceArgs": {"workers": 1}, "common-configs": ["model", "block-size", "max-model-len"]}}
+true, "router": "random", "tensor-parallel-size": 1, "ServiceArgs": {"workers": 1}}}
 """
     service_config = ServiceConfig.get_instance()
     vllm_worker_args = service_config.as_args("VllmWorker")
