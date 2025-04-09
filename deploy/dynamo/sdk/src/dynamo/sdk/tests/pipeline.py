f06e9e6da5a342f54c9504a99d09b4cd6ed20376
jon-chuang <9093549+jon-chuang@users.noreply.github.com>
2025-04-08 21:51:48 -0700

    fix test

--- deploy/dynamo/sdk/src/dynamo/sdk/tests/pipeline.py
+++ deploy/dynamo/sdk/src/dynamo/sdk/tests/pipeline.py
@@ -86,15 +86,15 @@ class Backend2:
     backend = depends(Backend)
 
     def __init__(self) -> None:
-        print("Starting middle2")
+        print("Starting backend2")
 
     @dynamo_endpoint()
     async def generate(self, req: RequestType):
         """Forward requests to backend."""
 
         req_text = req.text
-        print(f"Middle2 received: {req_text}")
-        text = f"{req_text}-mid2"
+        print(f"Backend2 received: {req_text}")
+        text = f"{req_text}-back2"
         next_request = RequestType(text=text).model_dump_json()
         print(next_request)
 
@@ -136,11 +136,11 @@ class Frontend:
         print(f"Frontend received type: {type(text)}")
         txt = RequestType(text=text)
         print(f"Frontend sending: {type(txt)}")
-        if self.backend:
-            async for back_resp in self.backend.generate(txt.model_dump_json()):
-                print(f"Frontend received back_resp: {back_resp}")
-                yield f"Frontend: {back_resp}"
-        else:
+        if self.middle:
             async for mid_resp in self.middle.generate(txt.model_dump_json()):
                 print(f"Frontend received mid_resp: {mid_resp}")
                 yield f"Frontend: {mid_resp}"
+        else:
+            async for back_resp in self.backend.generate(txt.model_dump_json()):
+                print(f"Frontend received back_resp: {back_resp}")
+                yield f"Frontend: {back_resp}"
