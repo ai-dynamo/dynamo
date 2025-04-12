1b7527dc9c5735a3a37e709ae31dfa48c448f3bf
Kevin Cheung <81500899+KevinCheung2259@users.noreply.github.com>
2025-04-12 23:20:57 +0800

    Update processor.py
    Signed-off-by: Kevin Cheung <81500899+KevinCheung2259@users.noreply.github.com>

--- examples/llm/components/processor.py
+++ examples/llm/components/processor.py
@@ -104,7 +104,7 @@ class Processor(ProcessMixIn):
         # Use the put method to update the value
         await self.etcd_kv_cache.put(
             "router",
-            str(self.engine_args.router).encode()
+            (self.engine_args.router).encode()
         )
 
     async def _generate(
