e5dbaf996b93939db053931e273b515ba982d46e
ishandhanani <ishandhanani@gmail.com>
2025-04-13 18:54:57 -0700

    bump

--- examples/llm/utils/vllm.py
+++ examples/llm/utils/vllm.py
@@ -14,11 +14,11 @@
 # limitations under the License.
 
 # TODO: rename to avoid ambiguity with vllm package
-
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.utils import FlexibleArgumentParser
 
 from dynamo.sdk.lib.config import ServiceConfig
+
 
 def parse_vllm_args(service_name, prefix) -> AsyncEngineArgs:
     config = ServiceConfig.get_instance()
