6e92a188f505a63c1455a64ea1b6844a0e686e4d
Hannah Zhang <hannahz@nvidia.com>
2025-04-28 14:43:01 -0700

    lint: pre-commit

--- deploy/dynamo/sdk/src/dynamo/sdk/cli/deployment.py
+++ deploy/dynamo/sdk/src/dynamo/sdk/cli/deployment.py
@@ -306,7 +306,9 @@ def create(
         ..., "--endpoint", "-e", help="Dynamo Cloud endpoint", envvar="DYNAMO_CLOUD"
     ),
     envs: Optional[List[str]] = typer.Option(
-        None, "--env", help="Environment variable(s) to set (format: KEY=VALUE). Note: These environment variables will be set on ALL services in your Dynamo pipeline."
+        None,
+        "--env",
+        help="Environment variable(s) to set (format: KEY=VALUE). Note: These environment variables will be set on ALL services in your Dynamo pipeline.",
     ),
 ) -> None:
     """Create a deployment on Dynamo Cloud.
