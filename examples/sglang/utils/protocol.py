a2ed5a45b104abce89c99c309ea7d3a111443870
ishandhanani <ishandhananai@gmail.com>
2025-05-04 22:44:37 +0000

    bump

--- examples/sglang/utils/protocol.py
+++ examples/sglang/utils/protocol.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Dict, Any
 
 class SGLangGenerateRequest(BaseModel):
-    # Wrapper around the GenerateReqInput which is the input to the SGLang engine
+    # Wrapper around the GenerateReqInput which is the input to SGLang engine
     request_id: str
     input_ids: list[int]
     sampling_params: dict
