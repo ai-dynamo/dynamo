188bef57354dce3644a21dddd4c94b6b59f78c15
Hannah Zhang <hannahz@nvidia.com>
2025-05-21 05:33:53 -0700

    feat: ignore mypy errors in generate_docs.py

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# type: ignore  # Ignore all mypy errors in this file
 
 # SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
