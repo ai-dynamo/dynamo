8b31ba2640e65d7e623334e1101fbdcef3c1c843
mohammedabdulwahhab <furkhan324@berkeley.edu>
2025-05-28 09:53:08 -0700

    fix: fix

--- deploy/sdk/src/dynamo/sdk/__init__.py
+++ deploy/sdk/src/dynamo/sdk/__init__.py
@@ -13,10 +13,15 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import warnings
 from typing import Any
 
-from bentoml import on_shutdown as async_on_shutdown
+# Suppress warning from setuptools caused by bentoml
+# TODO: Remove this line after the bentoml import is removed from this file
+warnings.filterwarnings("ignore", category=UserWarning, message=".*pkg_resources.*")
 
+# flake8: noqa: E402
+from bentoml import on_shutdown as async_on_shutdown
 from dynamo.sdk.core.decorators.endpoint import api, endpoint
 from dynamo.sdk.core.lib import DYNAMO_IMAGE, depends, liveness, readiness, service
 from dynamo.sdk.lib.decorators import async_on_start
