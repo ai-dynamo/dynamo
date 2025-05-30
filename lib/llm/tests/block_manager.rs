dcab68f3afcf862ec3142515f61fc441424ef6f8
Olga Andreeva <oandreeva@nvidia.com>
2025-05-30 14:52:12 -0700

    moving worker task out of impl

--- lib/llm/tests/block_manager.rs
+++ lib/llm/tests/block_manager.rs
@@ -279,7 +279,10 @@ pub mod llm_kvbm {
     impl DynamoKvbmRuntimeConfigBuilder {
         pub fn build(self) -> Result<kvbm::config::KvManagerRuntimeConfig> {
             let (runtime, nixl) = self.build_internal()?.dissolve();
-            let worker_id = runtime.primary_lease().unwrap().id() as u64;
+            let worker_id = runtime
+                .primary_lease()
+                .unwrap()
+                .id() as u64;
             Ok(kvbm::config::KvManagerRuntimeConfig::builder()
                 .worker_id(worker_id)
                 .cancellation_token(runtime.primary_token().child_token())
@@ -310,8 +313,12 @@ pub mod llm_kvbm {
     impl DynamoEventManager {
         pub fn new(component: Arc<KVBMDynamoRuntimeComponent>) -> Self {
             let (tx, rx) = mpsc::unbounded_channel();
-            let worker_id = component.drt().primary_lease().unwrap().id() as u64;
-            Self::worker_task(component, rx);
+            let worker_id = component
+                .drt()
+                .primary_lease()
+                .unwrap()
+                .id() as u64;
+            worker_task(component, rx);
             Self {
                 tx,
                 worker_identifier: worker_id,
@@ -321,70 +328,70 @@ pub mod llm_kvbm {
         pub fn publisher(self: &Arc<Self>) -> Publisher {
             Publisher::new(self.clone())
         }
+    }
 
-        // Worker task to receive and process messages
-        // TODO[oandreeva] The potential issue with the worker task
-        // being a slow subscriber. Needs to be perf tested and improved.
-        pub fn worker_task(
-            component: Arc<KVBMDynamoRuntimeComponent>,
-            mut rx: mpsc::UnboundedReceiver<Event>,
-        ) {
-            let mut event_id_counter = 0;
-            let component_clone = component.clone();
-            _ = component.drt().runtime().secondary().spawn(async move {
-                while let Some(event) = rx.recv().await {
-                    let event_id = event_id_counter;
-                    event_id_counter += 1;
-                    match event {
-                        Event::RegisterMultiple {
-                            blocks,
-                            worker_identifier,
-                        } => {
-                            let parent_hash = blocks.first().and_then(|(_, _, parent)| *parent);
-                            let store_data = KvCacheStoreData {
-                                blocks: blocks
-                                    .iter()
-                                    .map(|(sequence_hash, block_hash, _parent_sequence_hash)| {
-                                        KvCacheStoredBlockData {
-                                            block_hash: ExternalSequenceBlockHash(*sequence_hash),
-                                            tokens_hash: LocalBlockHash(*block_hash),
-                                        }
-                                    })
-                                    .collect(),
-                                parent_hash: parent_hash.map(ExternalSequenceBlockHash),
-                            };
-                            let data = KvCacheEventData::Stored(store_data);
-                            let event = KvCacheEvent { event_id, data };
-                            let router_event = RouterEvent::new(worker_identifier as i64, event);
-                            if let Err(e) = component_clone
-                                .batch_tx
-                                .send(PublisherEvent::Store(router_event))
-                            {
-                                tracing::warn!("Failed to send event to batch channel: {:?}", e);
-                            }
+    // Worker task to receive and process messages
+    // TODO[oandreeva] The potential issue with the worker task
+    // being a slow subscriber. Needs to be perf tested and improved.
+    pub fn worker_task(
+        component: Arc<KVBMDynamoRuntimeComponent>,
+        mut rx: mpsc::UnboundedReceiver<Event>,
+    ) {
+        let mut event_id_counter = 0;
+        let component_clone = component.clone();
+        _ = component.drt().runtime().secondary().spawn(async move {
+            while let Some(event) = rx.recv().await {
+                let event_id = event_id_counter;
+                event_id_counter += 1;
+                match event {
+                    Event::RegisterMultiple {
+                        blocks,
+                        worker_identifier,
+                    } => {
+                        let parent_hash = blocks.first().and_then(|(_, _, parent)| *parent);
+                        let store_data = KvCacheStoreData {
+                            blocks: blocks
+                                .iter()
+                                .map(|(sequence_hash, block_hash, _parent_sequence_hash)| {
+                                    KvCacheStoredBlockData {
+                                        block_hash: ExternalSequenceBlockHash(*sequence_hash),
+                                        tokens_hash: LocalBlockHash(*block_hash),
+                                    }
+                                })
+                                .collect(),
+                            parent_hash: parent_hash.map(ExternalSequenceBlockHash),
+                        };
+                        let data = KvCacheEventData::Stored(store_data);
+                        let event = KvCacheEvent { event_id, data };
+                        let router_event = RouterEvent::new(worker_identifier as i64, event);
+                        if let Err(e) = component_clone
+                            .batch_tx
+                            .send(PublisherEvent::Store(router_event))
+                        {
+                            tracing::warn!("Failed to send event to batch channel: {:?}", e);
                         }
-                        Event::Release {
-                            sequence_hash,
-                            worker_identifier,
-                        } => {
-                            let event = KvCacheEvent {
-                                event_id,
-                                data: KvCacheEventData::Removed(KvCacheRemoveData {
-                                    block_hashes: vec![ExternalSequenceBlockHash(sequence_hash)],
-                                }),
-                            };
-                            let router_event = RouterEvent::new(worker_identifier as i64, event);
-                            if let Err(e) = component_clone
-                                .batch_tx
-                                .send(PublisherEvent::Remove(router_event))
-                            {
-                                tracing::warn!("Failed to send event to batch channel: {:?}", e);
-                            }
+                    }
+                    Event::Release {
+                        sequence_hash,
+                        worker_identifier,
+                    } => {
+                        let event = KvCacheEvent {
+                            event_id,
+                            data: KvCacheEventData::Removed(KvCacheRemoveData {
+                                block_hashes: vec![ExternalSequenceBlockHash(sequence_hash)],
+                            }),
+                        };
+                        let router_event = RouterEvent::new(worker_identifier as i64, event);
+                        if let Err(e) = component_clone
+                            .batch_tx
+                            .send(PublisherEvent::Remove(router_event))
+                        {
+                            tracing::warn!("Failed to send event to batch channel: {:?}", e);
                         }
                     }
                 }
-            });
-        }
+            }
+        });
     }
 
     impl EventManager for DynamoEventManager {}
