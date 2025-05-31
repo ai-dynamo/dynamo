17378df11a35f683ee840279a5be5e4a60c28eb1
hongkuan <hongkuanz@nvidia.com>
2025-05-30 17:42:27 -0700

    pc

--- examples/vllm_v0/components/worker.py
+++ examples/vllm_v0/components/worker.py
@@ -212,7 +212,8 @@ class VllmWorker:
                 prefill_queue_size = await prefill_queue.get_queue_size()
             disagg_router_decision = await self.disaggregated_router.prefill_remote(
                 len(request.token_ids),
-                (request.estimated_prefix_hit_num_blocks or 0) * self.engine_args.block_size,
+                (request.estimated_prefix_hit_num_blocks or 0)
+                * self.engine_args.block_size,
                 prefill_queue_size,
             )
         else:
