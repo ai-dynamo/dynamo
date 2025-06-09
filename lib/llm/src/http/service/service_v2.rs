11c31be4a86c97f59208b4d57266e4b144589646
Abrar Shivani <shivaniabrar@gmail.com>
2025-06-10 02:30:22 +0530

    Remove clones

--- lib/llm/src/http/service/service_v2.rs
+++ lib/llm/src/http/service/service_v2.rs
@@ -236,7 +236,7 @@ impl HttpServiceConfigBuilder {
                 }
             },
         ));
-        routes.push((chat_docs.clone(), chat_route.clone()));
+        routes.push((chat_docs, chat_route));
 
         // Add completions route with conditional middleware
         let state_cmpl = state.clone();
@@ -263,7 +263,7 @@ impl HttpServiceConfigBuilder {
                 }
             },
         ));
-        routes.push((cmpl_docs.clone(), cmpl_route.clone()));
+        routes.push((cmpl_docs, cmpl_route));
 
         // Add embeddings route with conditional middleware
         let (embed_docs, embed_route) = super::openai::embeddings_router(state.clone(), None);
@@ -288,7 +288,7 @@ impl HttpServiceConfigBuilder {
                 }
             },
         ));
-        routes.push((embed_docs.clone(), embed_route.clone()));
+        routes.push((embed_docs, embed_route));
 
         for (route_docs, route) in routes {
             router = router.merge(route);
