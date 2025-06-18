d6a77772ab2f8eb838f0f45afb268b214d31c635
ishandhanani <ishandhanani@gmail.com>
2025-06-18 07:25:54 +0000

    bump

--- examples/sglang/utils/gap_bench.sh
+++ examples/sglang/utils/gap_bench.sh
@@ -1,14 +1,30 @@
-genai-perf profile \
-  --input-file new.jsonl \
-  --extra-inputs payload.j2 \
-  --endpoint /v1/experimental/dynamo/completions \
-  --endpoint-type completions \
-  --url localhost:8000 \
-  --model deepseek-ai/DeepSeek-R1 \
-  --tokenizer deepseek-ai/DeepSeek-R1 \
-  --streaming \
-  --concurrency 16 \
-  --request-count 160 \
-  --warmup-request-count 16 \
-  -H 'Accept: text/event-stream'
+MODEL=deepseek-ai/DeepSeek-R1
+OSL=5
+INPUT_FILE=<path-to-input-file>
+HEAD_PREFILL_NODE_IP=<ip>
+PORT=8000
+ARTIFACT_DIR=/benchmarks/
 
+# concurrency 1 and 2 are for warmup
+for concurrency in 1 2 8192; do
+  genai-perf profile \
+    --model ${MODEL} \
+    --tokenizer ${MODEL} \
+    --endpoint-type completions \
+    --endpoint /v1/experimental/dynamo/completions \
+    --streaming \
+    --url ${HEAD_PREFILL_NODE_IP}:${PORT} \
+    --input-file ${INPUT_FILE} \
+    --extra-inputs max_tokens:${OSL} \
+    --extra-inputs min_tokens:${OSL} \
+    --extra-inputs ignore_eos:true \
+    --concurrency ${concurrency} \
+    --request-count ${concurrency} \
+    --random-seed 100 \
+    --artifact-dir ${ARTIFACT_DIR} \
+    -- \
+    -v -v \
+    --max-threads 256 \
+    -H 'Authorization: Bearer NOT USED' \
+    -H 'Accept: text/event-stream'
+done
