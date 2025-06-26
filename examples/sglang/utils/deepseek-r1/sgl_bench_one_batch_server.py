0d0588f0dcc231b247e6eb33a5511e66896c8196
ishandhanani <ishandhanani@gmail.com>
2025-06-26 17:26:27 +0000

    copyright

--- examples/sglang/utils/deepseek-r1/sgl_bench_one_batch_server.py
+++ examples/sglang/utils/deepseek-r1/sgl_bench_one_batch_server.py
@@ -1,3 +1,18 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # ruff: noqa: E722, F841
 """
 This is a modified version of the https://github.com/sgl-project/sglang/blob/main/python/sglang/bench_one_batch_server.py script that allows for benchmarking OpenAI batch completion endpoints. Dynamo has it's own
