a0181b8835cd8fe36b6ec40a3b86bac74283635f
ishandhanani <ishandhanani@gmail.com>
2025-07-01 19:06:29 +0000

    init

---
+++ examples/sglang/components/decode_worker_inc.py
@@ -0,0 +1,74 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+import logging
+import asyncio
+import uvloop
+import sys
+
+from dynamo.runtime.logging import configure_dynamo_logging
+from dynamo.runtime import DistributedRuntime, dynamo_worker
+
+import sglang as sgl
+from sglang.srt.server_args import ServerArgs
+from utils.protocol import DisaggPreprocessedRequest
+from utils.sgl_utils import parse_sglang_args_inc
+
+configure_dynamo_logging()
+
+class DecodeRequestHandler:
+    def __init__(self, engine: sgl.Engine):
+        self.engine = engine
+        logging.info("Decode request handler initialized")
+
+    async def generate(self, req: DisaggPreprocessedRequest):
+        g = await self.engine.async_generate(
+            input_ids=req.request.token_ids
+            if req.request.batch_token_ids is None
+            else req.request.batch_token_ids,
+            sampling_params=req.sampling_params,
+            stream=True,
+            bootstrap_host=req.bootstrap_host,
+            bootstrap_port=req.bootstrap_port,
+            bootstrap_room=req.bootstrap_room,
+        )
+
+        async for result in g:
+            yield result
+
+@dynamo_worker(static=False)
+async def worker(runtime: DistributedRuntime):
+    server_args = parse_sglang_args_inc(sys.argv[1:])
+    await init(runtime, server_args)
+
+
+async def init(runtime: DistributedRuntime, server_args: ServerArgs):
+    """Initialize decode worker"""
+
+    engine = sgl.Engine(server_args=server_args)
+
+    handler = DecodeRequestHandler(engine)
+
+    component = runtime.namespace("dynamo").component("decode")
+    await component.create_service()
+
+    endpoint = component.endpoint("generate")
+    await endpoint.serve_endpoint(handler.generate)
+
+if __name__ == "__main__":
+    uvloop.install()
+    asyncio.run(worker())
--- examples/sglang/utils/sgl_utils.py
+++ examples/sglang/utils/sgl_utils.py
@@ -21,7 +21,6 @@ from sglang.srt.server_args import ServerArgs
 from dynamo.sdk.cli.utils import reserve_free_port
 from dynamo.sdk.lib.config import ServiceConfig
 
-
 def parse_sglang_args(service_name, prefix) -> ServerArgs:
     config = ServiceConfig.get_instance()
     sglang_args = config.as_args(service_name, prefix=prefix)
@@ -34,6 +33,19 @@ def parse_sglang_args(service_name, prefix) -> ServerArgs:
     args = parser.parse_args(sglang_args)
     if not any(
         arg.startswith("--disaggregation-bootstrap-port") for arg in sglang_args
+    ):
+        args_dict = vars(args)
+        args_dict["disaggregation_bootstrap_port"] = bootstrap_port
+        args = Namespace(**args_dict)
+    return ServerArgs.from_cli_args(args)
+
+def parse_sglang_args_inc(args: list[str]) -> ServerArgs:
+    parser = argparse.ArgumentParser()
+    bootstrap_port = _reserve_disaggregation_bootstrap_port()
+    ServerArgs.add_cli_args(parser)
+    args = parser.parse_args(args)
+    if not any(
+        arg.startswith("--disaggregation-bootstrap-port") for arg in args
     ):
         args_dict = vars(args)
         args_dict["disaggregation_bootstrap_port"] = bootstrap_port
