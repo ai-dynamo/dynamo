6855639e215219182e443fd9857a6f9958debade
Guan Luo <gluo@nvidia.com>
2025-07-10 15:50:38 -0700

    fix: fix up

--- lib/llm/src/model_card/create.rs
+++ lib/llm/src/model_card/create.rs
@@ -163,12 +163,16 @@ impl PromptFormatterArtifact {
         // Some HF model (i.e. meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8)
         // stores the chat template as a separate file, we want to optionally store
         // the content if it exists and put the chat template into config as normalization
-        let template: anyhow::Result<Self> = Ok(Self::HfChatTemplate(
-            check_for_file(repo_id, "chat_template.jinja").await?,
-        ));
-        Ok(template
+        Ok(Self::chat_template_try_is_hf_repo(repo_id)
+            .await
             .with_context(|| format!("unable to extract prompt format from repo {}", repo_id))
             .ok())
+    }
+
+    async fn chat_template_try_is_hf_repo(repo: &str) -> anyhow::Result<Self> {
+        Ok(Self::HfChatTemplate(
+            check_for_file(repo, "chat_template.jinja").await?,
+        ))
     }
 
     async fn try_is_hf_repo(repo: &str) -> anyhow::Result<Self> {
