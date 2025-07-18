425884a0848598fdabfb1e77113dc04279f5e36c
Graham King <grahamk@nvidia.com>
2025-07-18 10:45:20 -0400

    fix: Still figuring out Rust<->Python enums

--- lib/bindings/python/src/dynamo/_core.pyi
+++ lib/bindings/python/src/dynamo/_core.pyi
@@ -1,19 +1,6 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-from enum import Enum
 from typing import (
     Any,
     AsyncGenerator,
@@ -830,12 +817,9 @@ class ModelType:
     """What type of request this model needs: Chat, Component or Backend (pre-processed)"""
     ...
 
-class RouterMode(Enum):
+class RouterMode:
     """Router mode for load balancing requests across workers"""
     ...
-    #RoundRobin: 'RouterMode'
-    #Random: 'RouterMode'
-    #KV: 'RouterMode'
 
 class RouterConfig:
     """How to route the request"""
