e46fe0167ab36873c60837fb288ed37e95f34359
tanmayv25 <tanmay2592@gmail.com>
2025-08-07 19:30:40 -0700

    Fix

--- components/backends/trtllm/performance_sweeps/submit_agg.sh
+++ components/backends/trtllm/performance_sweeps/submit_agg.sh
@@ -30,7 +30,7 @@ mtp=0
 concurrency_list="1 2 4 8 16 32 64 128 256 512"
 
 max_num_tokens=$(( ((mtp+1)*max_batch+ISL+128+63)/64*64 ))
-sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} ${concurrency_list} ${mtp} ${kind} ${ISL} ${OSL} ${MODEL_PATH} ${SERVED_MODEL_NAME} ${IMAGE}
+sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${kind} ${ISL} ${OSL} ${MODEL_PATH} ${SERVED_MODEL_NAME} ${IMAGE}
 
 # dep4
 max_batch=1024
@@ -41,11 +41,11 @@ mtp=0
 
 concurrency_list="32 64 128 256 512 1024"
 max_num_tokens=$(( ((mtp+1)*max_batch+ISL+128+63)/64*64 ))
-sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} ${concurrency_list} ${mtp} ${kind} ${ISL} ${OSL} ${MODEL_PATH} ${SERVED_MODEL_NAME} ${IMAGE}
+sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${kind} ${ISL} ${OSL} ${MODEL_PATH} ${SERVED_MODEL_NAME} ${IMAGE}
 
 concurrency_list="2048 4096"
 max_num_tokens=$(( ((mtp+1)*max_batch+ISL+128+63)/64*64 ))
-sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} ${concurrency_list} ${mtp} ${kind} ${ISL} ${OSL} ${MODEL_PATH} ${SERVED_MODEL_NAME} ${IMAGE}
+sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${kind} ${ISL} ${OSL} ${MODEL_PATH} ${SERVED_MODEL_NAME} ${IMAGE}
 
 
 # tep8
@@ -57,7 +57,7 @@ mtp=0
 
 concurrency_list="1 2 4 8 16 32 64 128 256 512"
 max_num_tokens=$(( ((mtp+1)*max_batch+ISL+128+63)/64*64 ))
-sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} ${concurrency_list} ${mtp} ${kind} ${ISL} ${OSL} ${MODEL_PATH} ${SERVED_MODEL_NAME} ${IMAGE}
+sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${kind} ${ISL} ${OSL} ${MODEL_PATH} ${SERVED_MODEL_NAME} ${IMAGE}
 
 # dep8
 max_batch=1024
