7b8706786106bf513540ea2494d8242cc4933194
tanmayv25 <tanmay2592@gmail.com>
2025-08-08 16:59:16 -0700

    Fix me

--- components/backends/trtllm/performance_sweeps/submit_agg.sh
+++ components/backends/trtllm/performance_sweeps/submit_agg.sh
@@ -17,6 +17,7 @@ IMAGE="${IMAGE:-""}"
 
 ISL="${ISL:-8150}"
 OSL="${OSL:-1024}"
+TASKS_PER_NODE="${TASKS_PER_NODE:-4}"
 
 kind='dynamo_agg'
 
@@ -28,11 +29,12 @@ tp_size=4
 ep_size=${tp_size}
 enable_attention_dp=false
 mtp=0
+nodes_count=$((tp_size/TASKS_PER_NODE))
 
 concurrency_list="1 2 4 8 16 32 64 128 256 512 1024 2048"
 
 max_num_tokens=$(( ((mtp+1)*max_batch+ISL+128+63)/64*64 ))
-sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${common_args}
+sbatch --nodes=${nodes_count} --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${common_args}
 
 # dep4
 max_batch=1024
@@ -40,14 +42,15 @@ tp_size=4
 ep_size=${tp_size}
 enable_attention_dp=true
 mtp=0
+nodes_count=$((tp_size/TASKS_PER_NODE))
 
 concurrency_list="32 64 128 256 512 1024"
 max_num_tokens=$(( ((mtp+1)*max_batch+ISL+128+63)/64*64 ))
-sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${common_args}
+sbatch --nodes=${nodes_count} --ntasks=${tp_size} --ntasks-per-node=${TASKS_PER_NODE} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${common_args}
 
 concurrency_list="2048 4096"
 max_num_tokens=$(( ((mtp+1)*max_batch+ISL+128+63)/64*64 ))
-sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${common_args}
+sbatch --nodes=${nodes_count} --ntasks=${tp_size} --ntasks-per-node=${TASKS_PER_NODE} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${common_args}
 
 
 # tep8
@@ -56,10 +59,11 @@ tp_size=8
 ep_size=${tp_size}
 enable_attention_dp=false
 mtp=0
+nodes_count=$((tp_size/TASKS_PER_NODE))
 
 concurrency_list="1 2 4 8 16 32 64 128 256 512 1024 2048"
 max_num_tokens=$(( ((mtp+1)*max_batch+ISL+128+63)/64*64 ))
-sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${common_args}
+sbatch --nodes=${nodes_count} --ntasks=${tp_size} --ntasks-per-node=${TASKS_PER_NODE} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${common_args}
 
 # dep8
 max_batch=1024
@@ -67,16 +71,17 @@ tp_size=8
 ep_size=${tp_size}
 enable_attention_dp=true
 mtp=0
+nodes_count=$((tp_size/TASKS_PER_NODE))
 
 concurrency_list="32 64 128 256 512 1024"
 max_num_tokens=$(( ((mtp+1)*max_batch+ISL+128+63)/64*64 ))
-sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${common_args}
+sbatch --nodes=${nodes_count} --ntasks=${tp_size} --ntasks-per-node=${TASKS_PER_NODE} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${common_args}
 
 concurrency_list="2048 4096"
 max_num_tokens=$(( ((mtp+1)*max_batch+ISL+128+63)/64*64 ))
-sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${common_args}
+sbatch --nodes=${nodes_count} --ntasks=${tp_size} --ntasks-per-node=${TASKS_PER_NODE} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${common_args}
 
 # New: dep8 concurrency greater than 4096 as a separate group
 concurrency_list="6144 8192"
 max_num_tokens=$(( ((mtp+1)*max_batch+ISL+128+63)/64*64 ))
-sbatch --nodes=1 --ntasks=${tp_size} --ntasks-per-node=${tp_size} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${common_args}
+sbatch --nodes=${nodes_count} --ntasks=${tp_size} --ntasks-per-node=${TASKS_PER_NODE} benchmark_agg.slurm ${tp_size} ${ep_size} ${max_batch} ${max_num_tokens} ${enable_attention_dp} "${concurrency_list}" ${mtp} ${common_args}
