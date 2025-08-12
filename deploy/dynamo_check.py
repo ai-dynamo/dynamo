b9a48eea492003abeaa194cad44ad9e645f0d17e
Keiven Chang <keivenchang@users.noreply.github.com>
2025-08-12 15:53:43 -0700

    Add copyright header

--- deploy/dynamo_check.py
+++ deploy/dynamo_check.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 """
 dynamo package checker, Python import tester, and usage guide.
 
