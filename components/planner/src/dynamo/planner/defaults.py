8628ed15baa0044fd4849b67d1c3990ccb786518
qimcis <chixie.mcisaac@gmail.com>
2025-08-17 21:12:50 -0400

    standardize namespace environment variable to DYN_NAMESPACE

--- components/planner/src/dynamo/planner/defaults.py
+++ components/planner/src/dynamo/planner/defaults.py
@@ -71,7 +71,7 @@ def _get_default_prometheus_endpoint(port: str, namespace: str):
 
 class SLAPlannerDefaults(BasePlannerDefaults):
     port = os.environ.get("DYNAMO_PORT", "8000")
-    namespace = os.environ.get("DYNAMO_NAMESPACE", "vllm-disagg-planner")
+    namespace = os.environ.get("DYN_NAMESPACE", "vllm-disagg-planner")
     prometheus_endpoint = _get_default_prometheus_endpoint(port, namespace)
     profile_results_dir = "profiling_results"
     isl = 3000  # in number of tokens
