03c1145b5073e0e6f615710f98dbdf041600a85a
ishandhanani <ishandhanani@gmail.com>
2025-08-17 18:05:34 -0700

    args

--- components/backends/sgl/src/dynamo/sgl/args.py
+++ components/backends/sgl/src/dynamo/sgl/args.py
@@ -11,20 +11,26 @@ from argparse import Namespace
 import sglang as sgl
 from sglang.srt.server_args import ServerArgs
 
-# dynamo args/keys should be added here 
-DYNAMO_ARGS = ["namespace", "component", "endpoint", "migration-limit"]
+DEFAULT_ENDPOINT = "dyn://dynamo.backend.generate"
+DYNAMO_ARGS = {
+    "endpoint": {
+        "flags": ["--endpoint"],
+        "type": str,
+        "default": DEFAULT_ENDPOINT,
+        "help": f"Dynamo endpoint string in 'dyn://namespace.component.endpoint' format. Default: {DEFAULT_ENDPOINT}",
+    },
+    "migration-limit": {
+        "flags": ["--migration-limit"],
+        "type": int,
+        "default": 0,
+        "help": "Maximum number of times a request may be migrated to a different engine worker",
+    },
+}
 
 class DynamoArgs:
-    def __init__(self) -> None:
-        pass
-
-    @classmethod
-    def create_dynamo_args(args) -> None:
-        # for a in args
-        # match against dynamo args
-        # set them as self.<>
-        # remove them from the args array before giving to sglang
-        pass
+    def __init__(self, endpoint: str, migration_limit: int) -> None:
+        self.endpoint = endpoint
+        self.migration_limit = migration_limit
 
 class DisaggregationMode(Enum):
     """
@@ -39,7 +45,7 @@ class Config:
     def __init__(self, server_args: ServerArgs, dynamo_args: DynamoArgs) -> None:
         self.server_args = server_args
         self.dynamo_args = dynamo_args
-        self.serving_strategy = self._set_serving_strategy()
+        self.serving_mode = self._set_serving_strategy()
 
     def _set_serving_strategy(self):
         if self.server_args.disaggregation_mode == "null":
@@ -51,20 +57,41 @@ class Config:
         else:
             logging.error("Cannot ascertain serving strategy. This shouldn't happen")
 
-# TODO: parse dynamo args first and figure out how to make it clean 
-# like the vllm. Make add that style of parser in the DynamoArgs class
-# and then do the stuff below after its been removed. Yes?
-def parse_cmd_line_args(args: list[str]) -> Config:
-    dynamo_args = DynamoArgs.create_dynamo_args(args)
+def parse_args(args: list[str] = None) -> Config:
+    """
+    Parse all arguments and return Config with server_args and dynamo_args
+    """
     parser = argparse.ArgumentParser()
+    
+    # Dynamo args
+    for info in DYNAMO_ARGS.values():
+        parser.add_argument(
+            info["flags"],
+            type=info["type"],
+            default=info["default"],
+            help=info["help"],
+        )
+    
+    # SGLang args
     bootstrap_port = _reserve_disaggregation_bootstrap_port()
     ServerArgs.add_cli_args(parser)
+
     parsed_args = parser.parse_args(args)
+    
+    # Auto-set bootstrap port if not provided
     if not any(arg.startswith("--disaggregation-bootstrap-port") for arg in args):
         args_dict = vars(parsed_args)
         args_dict["disaggregation_bootstrap_port"] = bootstrap_port
         parsed_args = Namespace(**args_dict)
+    
+    # Create dynamo args from parsed values
+    dynamo_args = DynamoArgs(
+        endpoint=parsed_args.endpoint,
+        migration_limit=parsed_args.migration_limit,
+    )
     server_args = ServerArgs.from_cli_args(parsed_args)
+    
+    # Create config
     return Config(server_args, dynamo_args)
 
 @contextlib.contextmanager
