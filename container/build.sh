b37c7a94f7f5ba2aa930950df67c2be4cbfc497d
Harrison King Saturley-Hall <hsaturleyhal@nvidia.com>
2025-08-19 13:43:16 -0400

    fix: use coderabbit suggestions on help doc
    Signed-off-by: Harrison King Saturley-Hall <hsaturleyhal@nvidia.com>

--- container/build.sh
+++ container/build.sh
@@ -363,9 +363,8 @@ show_image_options() {
 
 show_help() {
     echo "usage: build.sh"
-    echo "  [--base base image]"
     echo "  [--base-image-tag base image tag]"
-    echo "  [--platform platform for docker build"
+    echo "  [--platform platform for docker build]"
     echo "  [--framework framework one of ${!FRAMEWORKS[*]}]"
     echo "  [--tensorrtllm-pip-wheel-dir path to tensorrtllm pip wheel directory]"
     echo "  [--tensorrtllm-commit tensorrtllm commit to use for building the trtllm wheel if the wheel is not provided]"
