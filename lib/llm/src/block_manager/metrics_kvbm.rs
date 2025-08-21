a8f403dab59d02fbab85d06edd9c661eb21e316b
Ziqi Fan <ziqif@nvidia.com>
2025-08-21 16:11:06 -0700

    clean up

--- lib/llm/src/block_manager/metrics_kvbm.rs
+++ lib/llm/src/block_manager/metrics_kvbm.rs
@@ -1,6 +1,20 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use dynamo_runtime::metrics::MetricsRegistry;
 use prometheus::IntCounter;
-
 
 #[derive(Clone, Debug)]
 pub struct KvbmMetrics {
@@ -10,8 +24,19 @@ pub struct KvbmMetrics {
 
 impl KvbmMetrics {
     pub fn new(mr: &dyn MetricsRegistry) -> Self {
-        let offload_requests = mr.create_intcounter("offload_requests", "The number of offload requests", &[]).unwrap();
-        let save_kv_layer_requests = mr.create_intcounter("save_kv_layer_requests", "The number of save kv layer requests", &[]).unwrap();
-        Self { offload_requests, save_kv_layer_requests }
+        let offload_requests = mr
+            .create_intcounter("offload_requests", "The number of offload requests", &[])
+            .unwrap();
+        let save_kv_layer_requests = mr
+            .create_intcounter(
+                "save_kv_layer_requests",
+                "The number of save kv layer requests",
+                &[],
+            )
+            .unwrap();
+        Self {
+            offload_requests,
+            save_kv_layer_requests,
+        }
     }
 }
