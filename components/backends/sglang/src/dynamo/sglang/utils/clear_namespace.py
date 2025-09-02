29c7d050f9435278da56d2e23d7e3409dc1f7d08
Biswa Panda <biswa.panda@gmail.com>
2025-09-02 16:43:33 -0700

    feat: dynamo namespace isolation for backend component
    Signed-off-by: Biswa Panda <biswa.panda@gmail.com>

--- components/backends/sglang/src/dynamo/sglang/utils/clear_namespace.py
+++ components/backends/sglang/src/dynamo/sglang/utils/clear_namespace.py
@@ -4,6 +4,7 @@
 import argparse
 import asyncio
 import logging
+import os
 
 from dynamo.runtime import DistributedRuntime, EtcdKvCache, dynamo_worker
 from dynamo.runtime.logging import configure_dynamo_logging
@@ -24,6 +25,11 @@ async def clear_namespace(runtime: DistributedRuntime, namespace: str):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--namespace", type=str, required=True)
+    parser.add_argument(
+        "--namespace", type=str, required=False, default=os.environ.get("DYN_NAMESPACE")
+    )
     args = parser.parse_args()
+    assert (
+        args.namespace
+    ), "Missing namespace, either pass --namespace or set DYN_NAMESPACE"
     asyncio.run(clear_namespace(args.namespace))
