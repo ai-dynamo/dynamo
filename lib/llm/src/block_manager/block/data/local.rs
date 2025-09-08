60e41ef3a679ce972de78e7598509b16a8b23698
Dan Aloni <dan.aloni@vastdata.com>
2025-09-08 18:24:19 +0300

    Draft: kvbm - bigger page size for offload block pools only
    This change allows the host and disk pool to have larger blocks compared
    to the device block pool, with a fixed block size ratio between these
    two pool classes. During offload and onboarding of blocks, we need to unite
    small device blocks to large offload blocks or to split large offloaded
    blocks back to small device blocks.
    
    The advantage conferred by this change: more efficient management of
    offloaded blocks, especifically when transferring back and forth from
    disk, larger IOs can always be used.
    
    To do:
    
    - Fix onboarding from disk to device
    - Fix support for TRTLLM
    - Add unit tests
    
    Signed-off-by: Dan Aloni <dan.aloni@vastdata.com>

--- lib/llm/src/block_manager/block/data/local.rs
+++ lib/llm/src/block_manager/block/data/local.rs
@@ -2,12 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use super::*;
+use smallvec::SmallVec;
 
 /// Individual block storage
 #[derive(Debug)]
 pub struct LocalBlockData<S: Storage> {
     layout: Arc<dyn BlockLayout<StorageType = S>>,
-    block_idx: usize,
+    // The special case of multiple block IDx will only happen for small KV cache blocks on device.
+    // offloaded blocks will be large and `block_idxs.len() == 1` will hold for them.
+    block_idxs: SmallVec<[usize; 1]>,
     block_set_idx: usize,
     worker_id: WorkerID,
 }
@@ -16,7 +19,34 @@ impl<S: Storage> Clone for LocalBlockData<S> {
     fn clone(&self) -> Self {
         Self {
             layout: self.layout.clone(),
-            block_idx: self.block_idx,
+            block_idxs: self.block_idxs.clone(),
+            block_set_idx: self.block_set_idx,
+            worker_id: self.worker_id,
+        }
+    }
+}
+
+pub struct LocalBlockDataBase<S: Storage> {
+    layout: Arc<dyn BlockLayout<StorageType = S>>,
+    block_set_idx: usize,
+    worker_id: WorkerID,
+}
+
+impl<S: Storage> Clone for LocalBlockDataBase<S> {
+    fn clone(&self) -> Self {
+        Self {
+            layout: self.layout.clone(),
+            block_set_idx: self.block_set_idx,
+            worker_id: self.worker_id,
+        }
+    }
+}
+
+impl<S: Storage> LocalBlockDataBase<S> {
+    pub(crate) fn get_data(&self, block_idxs: SmallVec<[usize; 1]>) -> LocalBlockData<S> {
+        LocalBlockData {
+            layout: self.layout.clone(),
+            block_idxs,
             block_set_idx: self.block_set_idx,
             worker_id: self.worker_id,
         }
@@ -36,9 +66,17 @@ where
     ) -> Self {
         Self {
             layout,
-            block_idx,
+            block_idxs: [block_idx].iter().map(|x| *x).collect(),
             block_set_idx,
             worker_id,
+        }
+    }
+
+    pub(crate) fn base(&self) -> LocalBlockDataBase<S> {
+        LocalBlockDataBase {
+            layout: self.layout.clone(),
+            block_set_idx: self.block_set_idx,
+            worker_id: self.worker_id.clone(),
         }
     }
 }
@@ -49,7 +87,22 @@ where
 {
     #[inline(always)]
     fn block_id(&self) -> BlockId {
-        self.block_idx
+        if self.block_idxs.len() == 1 {
+            self.block_idxs[0]
+        } else {
+            tracing::error!("Backtrace: {}", std::backtrace::Backtrace::force_capture());
+            panic!("used LocalBlockData::block_id() for fragmented block");
+        }
+    }
+
+    #[inline(always)]
+    fn fragment_block_id(&self, idx: usize) -> BlockId {
+        if self.block_idxs.len() != 1 {
+            self.block_idxs[idx]
+        } else {
+            tracing::error!("Backtrace: {}", std::backtrace::Backtrace::force_capture());
+            panic!("used LocalBlockData::fragment_block_id() for non--fragmented block");
+        }
     }
 
     #[inline(always)]
@@ -69,6 +122,14 @@ where
 
     fn is_fully_contiguous(&self) -> bool {
         self.layout.layout_type() == LayoutType::FullyContiguous
+    }
+
+    fn is_fragmented(&self) -> bool {
+        self.block_idxs.len() != 1
+    }
+
+    fn num_fragments(&self) -> usize {
+        self.block_idxs.len()
     }
 
     fn num_layers(&self) -> usize {
@@ -104,7 +165,22 @@ impl<S: Storage> BlockDataViews<S> for LocalBlockData<S> {
     ) -> BlockResult<view::LayerView<'_, S>> {
         let mr = self
             .layout
-            .memory_region(self.block_idx, layer_idx, outer_idx)?;
+            .memory_region(self.block_id(), layer_idx, outer_idx)?;
+        let storage_type = mr.storage_type();
+        unsafe { view::LayerView::new(self, mr.addr(), mr.size(), storage_type) }
+    }
+
+    fn local_layer_view_fragment(
+        &self,
+        fragment_idx: usize,
+        layer_idx: usize,
+        outer_idx: usize,
+    ) -> BlockResult<view::LayerView<'_, S>> {
+        let mr = self.layout.memory_region(
+            self.fragment_block_id(fragment_idx),
+            layer_idx,
+            outer_idx,
+        )?;
         let storage_type = mr.storage_type();
         unsafe { view::LayerView::new(self, mr.addr(), mr.size(), storage_type) }
     }
@@ -116,13 +192,27 @@ impl<S: Storage> BlockDataViews<S> for LocalBlockData<S> {
     ) -> BlockResult<view::LayerViewMut<'_, S>> {
         let mr = self
             .layout
-            .memory_region(self.block_idx, layer_idx, outer_idx)?;
+            .memory_region(self.block_id(), layer_idx, outer_idx)?;
+        unsafe { view::LayerViewMut::new(self, mr.addr(), mr.size(), mr.storage_type()) }
+    }
+
+    fn local_layer_view_fragment_mut(
+        &mut self,
+        fragment_idx: usize,
+        layer_idx: usize,
+        outer_idx: usize,
+    ) -> BlockResult<view::LayerViewMut<'_, S>> {
+        let mr = self.layout.memory_region(
+            self.fragment_block_id(fragment_idx),
+            layer_idx,
+            outer_idx,
+        )?;
         unsafe { view::LayerViewMut::new(self, mr.addr(), mr.size(), mr.storage_type()) }
     }
 
     fn local_block_view(&self) -> BlockResult<view::BlockView<'_, S>> {
         if self.is_fully_contiguous() {
-            let mr = self.layout.memory_region(self.block_idx, 0, 0)?;
+            let mr = self.layout.memory_region(self.block_id(), 0, 0)?;
             let offset = mr.addr();
             let size = mr.size()
                 .checked_mul(self.num_layers())
@@ -144,7 +234,7 @@ impl<S: Storage> BlockDataViews<S> for LocalBlockData<S> {
 
     fn local_block_view_mut(&mut self) -> BlockResult<view::BlockViewMut<'_, S>> {
         if self.is_fully_contiguous() {
-            let mr = self.layout.memory_region(self.block_idx, 0, 0)?;
+            let mr = self.layout.memory_region(self.block_id(), 0, 0)?;
             let offset = mr.addr();
             let size = mr.size()
                 .checked_mul(self.num_layers())
