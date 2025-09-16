b5959bf253a33c6033ef567abb67cace6d24ba89
Elnifio <elnifio0519@gmail.com>
2025-09-16 16:11:31 -0700

    adds header
    Signed-off-by: Elnifio <elnifio0519@gmail.com>

--- components/backends/sglang/slurm_jobs/scripts/vllm/backend_request_func.py
+++ components/backends/sglang/slurm_jobs/scripts/vllm/backend_request_func.py
@@ -1,4 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
+# pytest: skip-file
 
 import json
 import os
