317b2ebdc5fffd1fab8b437d826fb79bd4c73045
Ziqi Fan <ziqif@nvidia.com>
2025-09-25 15:59:28 -0700

    test
    Signed-off-by: Ziqi Fan <ziqif@nvidia.com>

--- components/backends/vllm/launch/disagg_kvbm.sh
+++ components/backends/vllm/launch/disagg_kvbm.sh
@@ -12,11 +12,11 @@ python -m dynamo.frontend --router-mode kv --http-port=8000 &
 CUDA_VISIBLE_DEVICES=0 python3 -m dynamo.vllm --model qwen/qwen3-0.6b --enforce-eager &
 
 # wait for decode worker to initialize
-sleep 20
+sleep 60
 
 # run prefill worker on GPU 1 with KVBM
 # NOTE: remove --enforce-eager for production use
-DYN_KVBM_CPU_CACHE_GB=4 \
+DYN_LOG=debug DYN_KVBM_CPU_CACHE_GB=4 \
 CUDA_VISIBLE_DEVICES=1 \
   python3 -m dynamo.vllm \
     --model qwen/qwen3-0.6b \
