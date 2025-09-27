28ff96218fff24ffb9edb799e7ca87877753ff49
Paul Li <zhixiong2008@gmail.com>
2025-09-27 00:54:38 -0700

    Update test_router_e2e_with_mockers.py
    Signed-off-by: Paul Li <zhixiong2008@gmail.com>

--- tests/router/test_router_e2e_with_mockers.py
+++ tests/router/test_router_e2e_with_mockers.py
@@ -671,7 +671,7 @@ def test_kv_push_router_bindings(request, runtime_services, predownload_tokenize
 
 
         # Wait for mockers to be ready
-        asyncio.run(send_request_to_specified_mocker_instence())
+        asyncio.run(send_request_to_specified_mocker_instence(mockers))
 
         # Run the async test
         async def test_kv_push_router():
