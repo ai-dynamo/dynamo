69ce0cc56f47b9be64db5d1feff538136702e84f
William Arnold <7565007+Aphoh@users.noreply.github.com>
2025-10-04 10:54:27 -0700

    Fix import ordering
    Signed-off-by: William Arnold <7565007+Aphoh@users.noreply.github.com>

--- components/src/dynamo/sglang/main.py
+++ components/src/dynamo/sglang/main.py
@@ -9,8 +9,8 @@ import sys
 import sglang as sgl
 import uvloop
 
-from dynamo.llm import ModelInput, ModelType
 from dynamo.common.config_dump import dump_config
+from dynamo.llm import ModelInput, ModelType
 from dynamo.runtime import DistributedRuntime, dynamo_worker
 from dynamo.runtime.logging import configure_dynamo_logging
 from dynamo.sglang.args import Config, DisaggregationMode, parse_args
