c37fe483314d89748acccff1b8205b506696badc
krishung5 <krish@nvidia.com>
2025-10-06 15:32:24 -0700

    fixup

--- components/src/dynamo/sglang/multimodal_utils/__init__.py
+++ components/src/dynamo/sglang/multimodal_utils/__init__.py
@@ -1,15 +1,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-"""
-Multimodal utilities for SGLang.
-
-This package contains utilities for processing multimodal (image/video) inputs
-for SGLang models.
-"""
-
 from dynamo.sglang.multimodal_utils.multimodal_chat_processor import (
-    extract_image_video_urls,
     multimodal_request_to_sglang,
     process_sglang_stream_response,
 )
@@ -19,7 +11,6 @@ from dynamo.sglang.multimodal_utils.multimodal_encode_utils import (
 from dynamo.sglang.multimodal_utils.multimodal_image_loader import ImageLoader
 
 __all__ = [
-    "extract_image_video_urls",
     "multimodal_request_to_sglang",
     "process_sglang_stream_response",
     "encode_image_embeddings",
