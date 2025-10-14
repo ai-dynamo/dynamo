b0319b060dcb7e4f88042bd5c1c7e28a284f0d86
hongkuanz <hongkuanz@nvidia.com>
2025-10-14 15:20:58 -0700

    return type
    Signed-off-by: hongkuanz <hongkuanz@nvidia.com>

--- benchmarks/profiler/utils/profiler_argparse.py
+++ benchmarks/profiler/utils/profiler_argparse.py
@@ -56,7 +56,7 @@ def parse_config_string(config_str: str) -> Dict[str, Any]:
     )
 
 
-def create_profiler_parser() -> argparse.ArgumentParser:
+def create_profiler_parser() -> argparse.Namespace:
     """
     Create argument parser with support for YAML config string.
     
