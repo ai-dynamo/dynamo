53a6fe5e33a854c8ad3f3a042c52c27f1ccda67d
tanmayv25 <tanmay2592@gmail.com>
2025-10-15 18:38:22 -0700

    Update for newer TRTLLM

--- components/src/dynamo/trtllm/main.py
+++ components/src/dynamo/trtllm/main.py
@@ -182,7 +182,6 @@ async def init(runtime: DistributedRuntime, config: Config):
         "pipeline_parallel_size": config.pipeline_parallel_size,
         "moe_expert_parallel_size": config.expert_parallel_size,
         "backend": Backend.PYTORCH,
-        "skip_tokenizer_init": True,
         "build_config": build_config,
         "kv_cache_config": kv_cache_config,
         "gpus_per_node": gpus_per_node,
