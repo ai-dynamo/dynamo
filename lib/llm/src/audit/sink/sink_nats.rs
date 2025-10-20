7851a76ac526d8ea2f474e809d5e7e6ffd32c9b3
Ryan Lempka <rlempka@nvidia.com>
2025-10-20 15:36:05 +0000

    fix: copyright format
    Signed-off-by: Ryan Lempka <rlempka@nvidia.com>

--- lib/llm/src/audit/sink/sink_nats.rs
+++ lib/llm/src/audit/sink/sink_nats.rs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA
+// SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 use async_nats::jetstream;
