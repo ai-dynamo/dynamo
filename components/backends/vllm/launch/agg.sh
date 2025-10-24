3a5041e76eb8eedf85b7db3a9bf1b73138ce3325
Keiven Chang <keivenchang@users.noreply.github.com>
2025-10-24 02:08:20 +0000

    remove --enable-metrics, vllm does not need it
    Signed-off-by: Keiven Chang <keivenchang@users.noreply.github.com>

--- components/backends/vllm/launch/agg.sh
+++ components/backends/vllm/launch/agg.sh
@@ -10,4 +10,4 @@ python -m dynamo.frontend --http-port=8000 &
 # run worker
 # --enforce-eager is added for quick deployment. for production use, need to remove this flag
 DYN_SYSTEM_ENABLED=true DYN_SYSTEM_PORT=8081 \
-    python -m dynamo.vllm --model Qwen/Qwen3-0.6B --enforce-eager --connector none --enable-metrics
+    python -m dynamo.vllm --model Qwen/Qwen3-0.6B --enforce-eager --connector none
