c628acafe5009e1682d81075ec84eef8b4b53d62
Kyle McGill <kmcgill@nvidia.com>
2025-11-11 16:40:46 -0800

    adding correct pytest markers

--- tests/ci/test_build_sh.py
+++ tests/ci/test_build_sh.py
@@ -19,7 +19,7 @@ from pathlib import Path
 
 import pytest
 
-pytestmark = [pytest.mark.unit, pytest.mark.ci, pytest.mark.cpu_only]
+pytestmark = [pytest.mark.unit, pytest.mark.pre_merge]
 
 
 @pytest.fixture
