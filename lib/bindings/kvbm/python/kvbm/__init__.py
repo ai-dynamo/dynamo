ac992540b09af6a03527fbca579d83edf9b22ddd
Tushar Sharma <tusharma@nvidia.com>
2025-11-18 15:52:09 -0800

    fix precommit failures
    Signed-off-by: Tushar Sharma <tusharma@nvidia.com>

--- lib/bindings/kvbm/python/kvbm/__init__.py
+++ lib/bindings/kvbm/python/kvbm/__init__.py
@@ -3,10 +3,12 @@
 
 # flake8: noqa
 import logging
+
 logger = logging.getLogger(__name__)
 
 # nixl needs to be loaded before any other imports to ensure that the nixl shared object is available for the KVBM core.
 import nixl
+
 logger.info(f"Loaded nixl API module: {nixl._api}")
 
 from kvbm._core import BlockManager as BlockManager
