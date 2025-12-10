9fb368df94b6e0b6e8c6ec991ae819dbdfc06a48
Biswa Panda <biswa.panda@gmail.com>
2025-12-09 23:28:02 -0800

    update default request plane to tcp

--- components/src/dynamo/trtllm/utils/trtllm_utils.py
+++ components/src/dynamo/trtllm/utils/trtllm_utils.py
@@ -300,7 +300,7 @@ def cmd_line_args():
         "--request-plane",
         type=str,
         choices=["nats", "http", "tcp"],
-        default=os.environ.get("DYN_REQUEST_PLANE", "nats"),
+        default=os.environ.get("DYN_REQUEST_PLANE", "tcp"),
         help="Determines how requests are distributed from routers to workers. 'tcp' is fastest [nats|http|tcp]",
     )
 
