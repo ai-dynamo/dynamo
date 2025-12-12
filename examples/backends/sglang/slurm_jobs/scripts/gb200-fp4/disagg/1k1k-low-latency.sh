75b4a35fca941e8c2f817f7c91b16e9681dde511
Ishan Dhanani <ishandhanani@gmail.com>
2025-12-12 20:26:20 +0000

    go

--- examples/backends/sglang/slurm_jobs/scripts/gb200-fp4/disagg/1k1k-low-latency.sh
+++ examples/backends/sglang/slurm_jobs/scripts/gb200-fp4/disagg/1k1k-low-latency.sh
@@ -80,6 +80,7 @@ if [ "$mode" = "prefill" ]; then
     if [[ -n "${DUMP_CONFIG_PATH}" ]]; then command_suffix="${command_suffix} --dump-config-to ${DUMP_CONFIG_PATH}"; fi
 
     PYTHONUNBUFFERED=1 \
+    FLASHINFER_WORKSPACE_BASE=/configs/flashinfer-cache \
     DYN_SKIP_SGLANG_LOG_FORMATTING=1 \
     SGLANG_USE_MESSAGE_QUEUE_BROADCASTER=0 \
     SGLANG_DISABLE_TP_MEMORY_INBALANCE_CHECK=1 \
@@ -136,6 +137,7 @@ elif [ "$mode" = "decode" ]; then
     if [[ -n "${DUMP_CONFIG_PATH}" ]]; then command_suffix="${command_suffix} --dump-config-to ${DUMP_CONFIG_PATH}"; fi
 
     PYTHONUNBUFFERED=1 \
+    FLASHINFER_WORKSPACE_BASE=/configs/flashinfer-cache \
     DYN_SKIP_SGLANG_LOG_FORMATTING=1 \
     SGLANG_USE_MESSAGE_QUEUE_BROADCASTER=0 \
     SGLANG_DISABLE_TP_MEMORY_INBALANCE_CHECK=1 \
