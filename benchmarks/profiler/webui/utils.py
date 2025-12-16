8bdb2f8666e7b7704d80fb1d753f9ad8db106af6
Hannah Zhang <hannahz@nvidia.com>
2025-12-16 13:18:41 -0800

    lint: linting
    Signed-off-by: Hannah Zhang <hannahz@nvidia.com>

--- benchmarks/profiler/webui/utils.py
+++ benchmarks/profiler/webui/utils.py
@@ -13,13 +13,13 @@ from pathlib import Path
 import gradio as gr
 import numpy as np
 import yaml
-
 from aiconfigurator.webapp.components.profiling import (
     create_performance_results_section,
     create_profiling_ui_components,
     inject_profiling_assets,
     load_profiling_javascript,
 )
+
 from benchmarks.profiler.utils.pareto import compute_pareto
 
 logger = logging.getLogger(__name__)
