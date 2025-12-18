117f03f9bf3cd6bec822e0e7228969e3ae08de48
acejilam <acejilam@gmail.com>
2025-12-18 13:00:54 +0800

    fix: lws main check
    Signed-off-by: acejilam <acejilam@gmail.com>

--- deploy/cloud/operator/internal/controller/dynamocomponentdeployment_controller.go
+++ deploy/cloud/operator/internal/controller/dynamocomponentdeployment_controller.go
@@ -604,14 +604,6 @@ func checkMainContainer(spec *corev1.PodSpec) error {
 			continue
 		}
 
-		if len(container.Command) == 0 {
-			return errors.New("container Command cannot be nil for LWS pod")
-		}
-
-		if len(container.Args) == 0 {
-			return errors.New("container Args cannot be empty for LWS pod")
-		}
-
 		mainContainerFound = true
 		break
 	}
