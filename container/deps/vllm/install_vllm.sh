b7a519c784a99ea07474bcd44b2866776e4868ff
Dmitry Tokarev <dtokarev@nvidia.com>
2025-12-20 02:00:43 -0500

    Fix cupy-cuda13x installation on CUDA 13 env
    Signed-off-by: Dmitry Tokarev <dtokarev@nvidia.com>

--- container/deps/vllm/install_vllm.sh
+++ container/deps/vllm/install_vllm.sh
@@ -148,9 +148,10 @@ elif [[ "$CUDA_VERSION_MAJOR" == "13" ]]; then
             --extra-index-url https://download.pytorch.org/whl/${TORCH_BACKEND} \
             https://github.com/vllm-project/vllm/releases/download/v${VLLM_VER}/vllm-${VLLM_VER}+${TORCH_BACKEND}-cp38-abi3-manylinux_2_31_x86_64.whl[flashinfer,runai] \
             --torch-backend=${TORCH_BACKEND}
+        uv pip uninstall cupy-cuda12x # remove cupy-cuda12x installed by vllm - bug in vllm packaging
+        uv pip install cupy-cuda13x
         uv pip install flashinfer-cubin==$FLASHINF_REF
         uv pip install flashinfer-jit-cache==$FLASHINF_REF --extra-index-url https://flashinfer.ai/whl/${TORCH_BACKEND}
-        echo "✓ vLLM installation completed"
     else
         echo "⚠ Skipping LMCache on ARM64 (compatibility issues, missing aarch64 wheels)"
         echo "Building vLLM from source for ${ARCH} architecture..."
@@ -158,6 +159,8 @@ elif [[ "$CUDA_VERSION_MAJOR" == "13" ]]; then
         uv pip install --index-strategy=unsafe-best-match --index https://download.pytorch.org/whl/ -r requirements/cuda.txt
         uv pip install setuptools_scm # required to build vLLM from source
         MAX_JOBS=${MAX_JOBS} uv pip install -v --no-build-isolation .
+        uv pip uninstall cupy-cuda12x # remove cupy-cuda12x installed by vllm - bug in vllm packaging
+        uv pip install cupy-cuda13x
     fi
 else
     echo "❌ Unsupported CUDA version for vLLM installation: ${CUDA_VERSION}"
