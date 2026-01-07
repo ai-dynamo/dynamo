37bcad432f60d99f04a70dc7cde3f8fe934dbd2c
jthomson04 <jwillthomson19@gmail.com>
2026-01-07 10:08:05 -0800

    fix curl healthcheck
    Signed-off-by: jthomson04 <jwillthomson19@gmail.com>

--- components/backends/trtllm/performance_sweeps/scripts/bench.sh
+++ components/backends/trtllm/performance_sweeps/scripts/bench.sh
@@ -113,7 +113,7 @@ report_ts=$(date +%s)
 
 while :; do
     # Curl timeout - our primary use case here is to launch it at the first node (localhost), so no timeout is needed.
-    curl_result=$(curl ${health_addr} 2>/dev/null)
+    curl_result=$(curl ${health_addr} 2>/dev/null || echo "curl error")
     # Python path - Use of `check_server_health.py` is self-constrained outside of any packaging.
     check_result=$(python3 $SCRIPTS_DIR/scripts/check_server_health.py $num_ctx_servers $num_gen_servers <<< $curl_result)
     if [[ $check_result == *"Model is ready."* ]]; then
