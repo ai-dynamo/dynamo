f42b6ce8af8bfadc213bd8ffa80aaf164f35d75e
Dmitry Tokarev <dtokarev@nvidia.com>
2026-01-09 12:27:17 -0500

    Added copyright and license headers to the test
    Signed-off-by: Dmitry Tokarev <dtokarev@nvidia.com>

--- tests/basic/test_cuda_version_consistency.py
+++ tests/basic/test_cuda_version_consistency.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+"""Integration test to check CUDA major version consistency across various packages."""
+
 import re
 import subprocess
 import pytest
