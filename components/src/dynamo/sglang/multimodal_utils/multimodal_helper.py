5bba82985ce4d6c03e5b96190b4d89fd0da6a29e
Krishnan Prashanth <kprashanth@nvidia.com>
2026-01-14 20:54:27 -0800

    Add copyright
    Signed-off-by: Krishnan Prashanth <kprashanth@nvidia.com>

--- components/src/dynamo/sglang/multimodal_utils/multimodal_helper.py
+++ components/src/dynamo/sglang/multimodal_utils/multimodal_helper.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 """Shared multimodal helper for aggregated and encode-worker paths."""
 
 import logging
