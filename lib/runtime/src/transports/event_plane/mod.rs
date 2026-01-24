2e60a8802e16ee5a5090194ca31fb935f7ac082c
Biswa Panda <biswa.panda@gmail.com>
2026-01-23 19:02:38 -0800

    feat: add ZMQ transport and dynamic discovery

--- lib/runtime/src/transports/event_plane/mod.rs
+++ lib/runtime/src/transports/event_plane/mod.rs
@@ -4,13 +4,17 @@
 //! Event Plane: Transport-agnostic pub/sub communication layer.
 
 mod codec;
+mod dynamic_subscriber;
 mod frame;
 mod nats_transport;
 mod traits;
 mod transport;
+mod zmq_transport;
 
 pub use codec::MsgpackCodec;
+pub use dynamic_subscriber::DynamicSubscriber;
 pub use frame::{Frame, FrameError, FrameHeader};
 pub use nats_transport::NatsTransport;
 pub use traits::{EventEnvelope, EventStream, TypedEventStream};
 pub use transport::{EventTransportRx, EventTransportTx, WireStream};
+pub use zmq_transport::{ZmqPubTransport, ZmqSubTransport};
