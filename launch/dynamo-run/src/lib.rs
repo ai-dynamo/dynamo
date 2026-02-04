6d70043dffe912eabd0e89d0c0cbb86c28aa1d37
PeaBrane <yanrpei@gmail.com>
2026-02-04 12:07:19 -0800

    update dynamo-run
    Signed-off-by: PeaBrane <yanrpei@gmail.com>

--- launch/dynamo-run/src/lib.rs
+++ launch/dynamo-run/src/lib.rs
@@ -165,7 +165,7 @@ async fn engine_for(
             let endpoint = local_model.endpoint_id().clone();
 
             let engine =
-                dynamo_llm::mocker::engine::make_mocker_engine(drt, endpoint, args).await?;
+                dynamo_llm::mocker::make_mocker_engine(drt, endpoint, args).await?;
 
             Ok(EngineConfig::InProcessTokens {
                 engine,
