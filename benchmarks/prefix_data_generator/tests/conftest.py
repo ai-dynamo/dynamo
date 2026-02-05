d5849505e27c0cf606b11b1eae9c6d95cc7db1cf
PeaBrane <yanrpei@gmail.com>
2026-02-04 23:34:53 -0800

    fix: add conftest.py to skip benchmark tests, revert pyproject.toml
    Signed-off-by: PeaBrane <yanrpei@gmail.com>

---
+++ benchmarks/prefix_data_generator/tests/conftest.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# Skip collection of tests in this directory - they require dependencies
+# not available in the main dynamo test environment.
+collect_ignore = ["test_synthesizer.py", "test_sampler.py"]
