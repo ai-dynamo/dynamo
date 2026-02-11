951f0cbc7a0fb96709c5f3f009ae8e449ee1ed91
richardhuo-nv <rihuo@nvidia.com>
2026-02-10 20:33:04 -0800

    fix build scripts

--- container/build_trtllm_wheel.sh
+++ container/build_trtllm_wheel.sh
@@ -92,8 +92,8 @@ grep "__version__" "$VERSION_FILE"
 
 if [ "$ARCH" = "amd64" ]; then
     # Need to build in the Triton Devel Image for NIXL support.
-    make -C docker tritondevel_build
-    make -C docker wheel_build DEVEL_IMAGE=tritondevel BUILD_WHEEL_OPTS='--extra-cmake-vars NIXL_ROOT=/opt/nvidia/nvda_nixl'
+    make -C docker tritondevel_build SHELL=/bin/bash
+    make -C docker wheel_build DEVEL_IMAGE=tritondevel BUILD_WHEEL_OPTS='--extra-cmake-vars NIXL_ROOT=/opt/nvidia/nvda_nixl' SHELL=/bin/bash
 else
     # NIXL backend is not supported on arm64 for TensorRT-LLM.
     # See here: https://github.com/NVIDIA/TensorRT-LLM/blob/main/docker/common/install_nixl.sh
--- docs/components/kvbm/kvbm_guide.md
+++ docs/components/kvbm/kvbm_guide.md
@@ -218,6 +218,7 @@ cd $DYNAMO_HOME/examples/backends/vllm
 
 ```bash
 # Build the Dynamo TensorRT-LLM container using commit ID 18e611da773026a55d187870ebcfa95ff00c8482. Note: This build can take a long time.
+rm -rf /tmp/TensorRT-LLM && \ 
 ./container/build.sh --framework trtllm --tensorrtllm-commit 18e611da773026a55d187870ebcfa95ff00c8482 --tensorrtllm-git-url https://github.com/NVIDIA/TensorRT-LLM.git
 
 # Launch the container
