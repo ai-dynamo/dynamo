2452b0b99433eefd5a3fea595434370ceee709eb
PeaBrane <yanrpei@gmail.com>
2026-02-19 18:05:45 +0000

    new bench
    Signed-off-by: PeaBrane <yanrpei@gmail.com>

--- lib/kv-router/run_bench.sh
+++ lib/kv-router/run_bench.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 cargo bench -p dynamo-kv-router --bench mooncake_bench --features bench -- \
     /home/ubuntu/dynamo/mooncake_trace.jsonl \
     --sweep \
-    --sweep-max-ms 100000 \
+    --sweep-max-ms 200000 \
     --sweep-min-ms 1000 \
-    --sweep-steps 10 \
+    --sweep-steps 12 \
     --trace-duplication-factor 4 \
     --num-gpu-blocks 16384 \
     --trace-length-factor 16 \
