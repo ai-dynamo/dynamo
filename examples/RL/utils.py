cfa44f919b57f734c31628f641fdd0b2cf99c8fc
Sean Choi <sechoi@nvidia.com>
2026-02-19 13:37:48 -0800

    copyright check
    Signed-off-by: Sean Choi <sechoi@nvidia.com>

--- examples/RL/utils.py
+++ examples/RL/utils.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 import time
 from dataclasses import dataclass, field
 from typing import Any, List
