10e2b086a5d639aea3cc4285ec3793b704da5829
hongkuanz <hongkuanz@nvidia.com>
2026-02-19 16:13:12 -0800

    copyright
    Signed-off-by: hongkuanz <hongkuanz@nvidia.com>

--- components/src/dynamo/profiler/utils/dgdr_v1beta1_types.py
+++ components/src/dynamo/profiler/utils/dgdr_v1beta1_types.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 """
 Auto-generated Pydantic models from v1beta1 DGDR Go types.
 
