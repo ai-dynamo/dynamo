408a249cc24c7ae8a6fecfded7a601ef8e34201e
Graham King <grahamk@nvidia.com>
2026-03-05 13:41:07 -0500

    fix(llm): Remove the llm -> mocker crate dependency
    The bindings now use the dynamo-mocker crate directly, instead of via a small function in dynamo-llm.
    
    Note that I had to comment out a block of code in local_model.rs which was overriding the model deployment card values. Ideally the mocker engine should set those like any other engine. We'll need to revisit.
    
    Signed-off-by: Graham King <grahamk@nvidia.com>

--- lib/bindings/python/rust/llm/entrypoint.rs
+++ lib/bindings/python/rust/llm/entrypoint.rs
@@ -18,9 +18,9 @@ use dynamo_llm::entrypoint::input::Input;
 use dynamo_llm::kv_router::KvRouterConfig as RsKvRouterConfig;
 use dynamo_llm::local_model::DEFAULT_HTTP_PORT;
 use dynamo_llm::local_model::{LocalModel, LocalModelBuilder};
-use dynamo_llm::mocker::protocols::MockEngineArgs;
 use dynamo_llm::model_card::ModelDeploymentCard as RsModelDeploymentCard;
 use dynamo_llm::types::openai::chat_completions::OpenAIChatCompletionsStreamingEngine;
+use dynamo_mocker::common::protocols::MockEngineArgs;
 use dynamo_runtime::discovery::ModelCardInstanceId as RsModelCardInstanceId;
 use dynamo_runtime::protocols::EndpointId;
 
@@ -419,12 +419,9 @@ async fn select_engine(
 
             let endpoint = local_model.endpoint_id().clone();
 
-            let engine = dynamo_llm::mocker::make_mocker_engine(
-                distributed_runtime.inner,
-                endpoint,
-                mocker_args,
-            )
-            .await?;
+            let engine =
+                dynamo_mocker::make_mocker_engine(distributed_runtime.inner, endpoint, mocker_args)
+                    .await?;
 
             RsEngineConfig::InProcessTokens {
                 engine,
