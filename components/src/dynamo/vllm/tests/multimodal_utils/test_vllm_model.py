1f0457f91afa0012e185e417d6d0fd7654236707
Guan Luo <41310872+GuanLuo@users.noreply.github.com>
2026-03-09 14:34:24 -0700

    chore: update test
    Signed-off-by: Guan Luo <41310872+GuanLuo@users.noreply.github.com>

--- components/src/dynamo/vllm/tests/multimodal_utils/test_vllm_model.py
+++ components/src/dynamo/vllm/tests/multimodal_utils/test_vllm_model.py
@@ -20,10 +20,11 @@ pytestmark = [
 class TestMultiModalUtils:
     def test_construct_qwen_decode_mm_data(self):
         max_rounds = int(torch.finfo(torch.float16).max) + 2
+        expected_image_grid_thw_tensor = torch.tensor([16, 16])
         for i in range(max_rounds):
             # Should not raise any exception
             try:
-                construct_qwen_decode_mm_data(
+                mm_data = construct_qwen_decode_mm_data(
                     image_grid_thw=[16, 16],
                     embeddings_shape=[2, 1024],
                     request_id=str(i),
@@ -32,3 +33,11 @@ class TestMultiModalUtils:
                 pytest.fail(
                     f"construct_qwen_decode_mm_data raised {type(e).__name__} on round {i}: {e}"
                 )
+            assert "image" in mm_data
+            assert "image_grid_thw" in mm_data["image"]
+            assert "image_embeds" in mm_data["image"]
+            assert torch.allclose(
+                mm_data["image"]["image_grid_thw"], expected_image_grid_thw_tensor
+            )
+            # Embedding values are randomly genearted as placehodler, we only check the shape
+            assert mm_data["image"]["image_embeds"].shape == (2, 1024)
