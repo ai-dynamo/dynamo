c0bc56dbf2337e9aca97d5711a91e7d9c3c82042
jthomson04 <jwillthomson19@gmail.com>
2026-03-09 12:55:48 -0700

    unblock everything
    Signed-off-by: jthomson04 <jwillthomson19@gmail.com>

--- tests/router/test_router_e2e_with_mockers.py
+++ tests/router/test_router_e2e_with_mockers.py
@@ -651,7 +651,6 @@ def test_mocker_two_kv_router(
         )
 
 
-@pytest.mark.skip(reason="Flaky, temporarily disabled")
 @pytest.mark.parametrize(
     "durable_kv_events", [False], indirect=True
 )  # Use NATS Core (local indexer)
