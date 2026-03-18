ffe18c05df27a55d4865cfe9a5281926620699ed
Anna Tchernych <atchernych@nvidia.com>
2026-03-18 16:45:58 -0700

    do not skip
    Signed-off-by: Anna Tchernych <atchernych@nvidia.com>

--- tests/deploy/test_deploy.py
+++ tests/deploy/test_deploy.py
@@ -227,7 +227,6 @@ async def test_deployment(
 
 # GAIE (Gateway API Inference Extension) deployment test
 @pytest.mark.gaie
-@pytest.mark.vllm
 @pytest.mark.k8s
 @pytest.mark.deploy
 @pytest.mark.post_merge
