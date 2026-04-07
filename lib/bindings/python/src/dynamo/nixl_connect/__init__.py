7020fd486dba87c7adb29b84525aff96ddf67627
Matej Kosec <mkosec@nvidia.com>
2026-04-07 19:24:29 +0000

    style: fix pre-commit formatting
    Signed-off-by: Matej Kosec <mkosec@nvidia.com>

--- lib/bindings/python/src/dynamo/nixl_connect/__init__.py
+++ lib/bindings/python/src/dynamo/nixl_connect/__init__.py
@@ -495,7 +495,11 @@ class ActiveOperation(AbstractOperation):
         """
         # Early return if the operation is already complete, errored, or cancelled.
         match self._status:
-            case OperationStatus.COMPLETE | OperationStatus.ERRORED | OperationStatus.CANCELLED:
+            case (
+                OperationStatus.COMPLETE
+                | OperationStatus.ERRORED
+                | OperationStatus.CANCELLED
+            ):
                 return self._status
 
         if self._xfer_hndl is None:
@@ -1472,7 +1476,11 @@ class PassiveOperation(AbstractOperation):
         """
         # Early return if the operation is already complete, errored, or cancelled.
         match self._status:
-            case OperationStatus.COMPLETE | OperationStatus.ERRORED | OperationStatus.CANCELLED:
+            case (
+                OperationStatus.COMPLETE
+                | OperationStatus.ERRORED
+                | OperationStatus.CANCELLED
+            ):
                 return self._status
 
         old_status = self._status
