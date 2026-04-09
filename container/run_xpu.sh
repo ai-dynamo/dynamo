f2b9dc5502a4deb6c0d146f7a1c1c0df578d337b
Hongming Zheng <hongming.zheng@intel.com>
2026-04-09 04:29:32 +0000

    minor
    Signed-off-by: Hongming Zheng <hongming.zheng@intel.com>

--- container/run_xpu.sh
+++ container/run_xpu.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""This script is to create docker container for xpu device"""
+
 set -e
 
 RUN_PREFIX=
