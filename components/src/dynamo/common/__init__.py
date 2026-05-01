60478855e886e2dd70df6f1c625ff4c0c9c0aee2
Keiven Chang <keivenchang@users.noreply.github.com>
2026-05-01 12:56:04 -0700

    test(validate): revalidate 1a930ddbd98
    Signed-off-by: Keiven Chang <keivenchang@users.noreply.github.com>

--- components/src/dynamo/common/__init__.py
+++ components/src/dynamo/common/__init__.py
@@ -25,3 +25,5 @@ except Exception:
         __version__ = "0.0.0+unknown"
 
 __all__ = ["__version__", "config_dump", "constants", "utils"]
+
+# revalidate 1a930ddbd98 2026-05-01T19:56:04Z
--- lib/runtime/src/runtime.rs
+++ lib/runtime/src/runtime.rs
@@ -391,3 +391,5 @@ impl Drop for RuntimeType {
         }
     }
 }
+
+// revalidate 1a930ddbd98 2026-05-01T19:56:04Z
