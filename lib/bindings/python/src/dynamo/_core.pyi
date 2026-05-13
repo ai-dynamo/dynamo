9be3449d927d60d127a3bf5c48dabc8951e839d2
PeaBrane <yanrpei@gmail.com>
2026-05-12 23:17:33 -0700

    fix router AIC MoE config plumbing
    Signed-off-by: PeaBrane <yanrpei@gmail.com>

--- lib/bindings/python/src/dynamo/_core.pyi
+++ lib/bindings/python/src/dynamo/_core.pyi
@@ -1274,6 +1274,9 @@ class AicPerfConfig:
         aic_model_path: str,
         aic_tp_size: int = 1,
         aic_backend_version: Optional[str] = None,
+        aic_moe_tp_size: Optional[int] = None,
+        aic_moe_ep_size: Optional[int] = None,
+        aic_attention_dp_size: Optional[int] = None,
     ) -> None:
         ...
 
