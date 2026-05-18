b28dc97d841aad364019a8c3f9aab2d6a86aba2a
Alec Flowers <aflowers@nvidia.com>
2026-05-18 15:15:50 -0700

    Revert "Make MM router overlap logs observable in CI"
    This reverts commit b414d232e22ae62a77f85957376ab35cbbfbe1c8.

--- tests/mm_router/test_mm_router_e2e.py
+++ tests/mm_router/test_mm_router_e2e.py
@@ -78,7 +78,6 @@ def _make_process_env(log_level: str = "debug", **extra) -> dict[str, str]:
     env["DYN_LOG"] = log_level
     env["DYN_NAMESPACE"] = NAMESPACE
     env["DYN_REQUEST_PLANE"] = "nats"
-    env["DYN_ROUTER_KV_OVERLAP_LOG_LEVEL"] = "info"
     env.update(extra)
     return env
 
