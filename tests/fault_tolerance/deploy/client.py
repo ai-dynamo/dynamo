ef474678d434d2cbf1a4e28a12fb7db41399bb6e
pvijayakrish <pvijayakrish@nvidia.com>
2026-06-29 14:23:43 -0700

    chore: upgrade the aiperf version in dynamo repo
    Signed-off-by: pvijayakrish <pvijayakrish@nvidia.com>

--- tests/fault_tolerance/deploy/client.py
+++ tests/fault_tolerance/deploy/client.py
@@ -339,8 +339,7 @@ def run_aiperf(
                 )
                 if success:
                     break  # Success - exit the retry loop
-            ## TODO: bug with aiperf git+https://github.com/ai-dynamo/aiperf.git@54cd6dc820bff8bfebc875da104e59d745e14f75
-            ## where sending a SIGINT on Mac can sometimes have an error code of -9 (SIGABRT) which results in profile_export_aiperf.json not being created
+            ## TODO: aiperf bug where sending a SIGINT on Mac can sometimes have an error code of -9 (SIGABRT) which results in profile_export_aiperf.json not being created
             elif result.returncode == -9 and continuous_load:
                 logger.warning(
                     f"""
