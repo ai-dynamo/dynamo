42baabe4dd15ac997c479eb12423d529fb596cb4
Qi Wang <qiwa@nvidia.com>
2026-06-30 13:33:16 -0700

    chore(multimodal): SPDX year 2026 on new files (drop 2025-2026)
    Signed-off-by: Qi Wang <qiwa@nvidia.com>

--- components/src/dynamo/vllm/multimodal_utils/async_vision_encoder.py
+++ components/src/dynamo/vllm/multimodal_utils/async_vision_encoder.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 """Serial async glue between the worker's event loop and a ``VisionEncoderBackend``.
