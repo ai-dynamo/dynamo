34608fd85dc456d618d77dc52d1d3e7ad8657211
Qi Wang <qiwa@nvidia.com>
2026-06-30 13:32:34 -0700

    chore(multimodal): SPDX year 2026 on new files (drop 2025-2026)

--- components/src/dynamo/vllm/multimodal_utils/vision_encoder_backend.py
+++ components/src/dynamo/vllm/multimodal_utils/vision_encoder_backend.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 """The author-written contract for a pluggable in-process vision encoder.
