e142102e890d0972f403b45d5bea6df1112cd174
PeaBrane <yanrpei@gmail.com>
2026-07-15 20:55:34 -0700

    fix(bindings): allow router config constructor arity
    Signed-off-by: PeaBrane <yanrpei@gmail.com>

--- lib/bindings/python/rust/llm/entrypoint.rs
+++ lib/bindings/python/rust/llm/entrypoint.rs
@@ -439,6 +439,7 @@ pub struct RouterConfig {
 impl RouterConfig {
     #[new]
     #[pyo3(signature = (mode, config=None, active_decode_blocks_threshold=None, active_prefill_tokens_threshold=None, active_prefill_tokens_threshold_frac=None, enforce_disagg=false, session_affinity_ttl_secs=None, session_affinity_replica_sync=false))]
+    #[allow(clippy::too_many_arguments)]
     pub fn new(
         mode: RouterMode,
         config: Option<KvRouterConfig>,
