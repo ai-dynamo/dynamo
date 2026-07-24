26e57432a9619965d1469419a38aab5e1cb9d7ae
Jie Hao <jihao@nvidia.com>
2026-07-24 19:20:31 -0400

    feat(http): serve classify and pooling requests
    Signed-off-by: Jie Hao <jihao@nvidia.com>

--- lib/llm/tests/http-service.rs
+++ lib/llm/tests/http-service.rs
@@ -285,6 +285,8 @@ fn compute_index(endpoint: &Endpoint, request_type: &RequestType, status: &Statu
         Endpoint::Completions => 0,
         Endpoint::ChatCompletions => 1,
         Endpoint::Embeddings => todo!(),
+        Endpoint::Classify => todo!(),
+        Endpoint::Pooling => todo!(),
         Endpoint::Responses => todo!(),
         Endpoint::AnthropicMessages => todo!(),
         Endpoint::Tensor => todo!(),
