4e2d39106c56fd3d4a9f18e95218d86307bc576c
Ishan Dhanani <ishandhanani@gmail.com>
2026-08-01 04:11:14 +0000

    perf(router): preserve default selection fast path
    Signed-off-by: Ishan Dhanani <ishandhanani@gmail.com>

--- lib/kv-router/src/lib.rs
+++ lib/kv-router/src/lib.rs
@@ -72,7 +72,8 @@ pub use scheduling::{
     WorkerSelectionPolicyError,
 };
 pub use selector::{
-    DefaultWorkerPicker, DefaultWorkerScorer, DefaultWorkerSelector, WorkerCandidates,
-    WorkerPicker, WorkerScorer, WorkerSelectionContext, WorkerSelectionPolicy, WorkerSelector,
+    DefaultWorkerPicker, DefaultWorkerScorer, DefaultWorkerSelector, WorkerCandidate,
+    WorkerCandidates, WorkerPicker, WorkerScorer, WorkerSelectionContext, WorkerSelectionPolicy,
+    WorkerSelector,
 };
 pub use tracking_hash::{TrackingHashAlgorithm, TrackingHashContext, TrackingHashScope};
