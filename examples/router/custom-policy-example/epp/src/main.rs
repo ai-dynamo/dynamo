1747553fbc4ed898c2c64a871fb8913798893068
Ishan Dhanani <ishandhanani@gmail.com>
2026-08-10 18:53:41 +0000

    fix(router): reject custom policy on in-process engines
    Signed-off-by: Ishan Dhanani <ishandhanani@gmail.com>

--- examples/router/custom-policy-example/epp/src/main.rs
+++ examples/router/custom-policy-example/epp/src/main.rs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 use dynamo_ext_proc::run_with_worker_selection_policy_registry;
