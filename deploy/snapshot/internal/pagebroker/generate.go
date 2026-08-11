3adf49a27db07f38a8a6b167534d3c111efc96b8
Dan Feigin <dfeigin@nvidia.com>
2026-08-11 16:35:48 +0300

    feat(snapshot): add typed PageBroker wire contract
    Signed-off-by: Dan Feigin <dfeigin@nvidia.com>

---
+++ deploy/snapshot/internal/pagebroker/generate.go
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package pagebroker
+
+//go:generate protoc --proto_path=../../pagebroker --go_out=. --go_opt=paths=source_relative ../../pagebroker/pagebroker.proto
