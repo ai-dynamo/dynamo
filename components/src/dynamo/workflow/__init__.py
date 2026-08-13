5ed180d815c64bcc7016ef64cd2163bb3ae6896d
furionw <qiwa@nvidia.com>
2026-08-13 14:56:17 -0700

    feat(workflow): execute graph and handler definitions

--- components/src/dynamo/workflow/__init__.py
+++ components/src/dynamo/workflow/__init__.py
@@ -1,19 +1,15 @@
 # SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-"""Authoring and canonical IR for Dynamo inference workflows."""
+"""Authoring, compilation, and execution for Dynamo inference workflows."""
 
 from dynamo.workflow.builder import StageDefinition, StageHandle, Workflow
-from dynamo.workflow.definition import StageRef, WorkflowDefinition, WorkflowHandler
 from dynamo.workflow.compiler import DeploymentSpec, compile_workflow
+from dynamo.workflow.definition import StageRef, WorkflowDefinition, WorkflowHandler
+from dynamo.workflow.executor import WorkflowContext, WorkflowExecutor
 from dynamo.workflow.ir import StageIR, WorkflowIR
 from dynamo.workflow.plan import EdgePlan, ExecutionPlan, LocalBinding
-from dynamo.workflow.runtime import (
-    StageContext,
-    StageRunner,
-    WorkflowExecutionError,
-    WorkflowExecutor,
-)
+from dynamo.workflow.runtime import StageContext, StageRunner, WorkflowExecutionError
 from dynamo.workflow.types import (
     StageContract,
     ValueRef,
@@ -36,6 +32,7 @@ __all__ = [
     "Workflow",
     "WorkflowDefinition",
     "WorkflowHandler",
+    "WorkflowContext",
     "WorkflowIR",
     "WorkflowExecutionError",
     "WorkflowExecutor",
