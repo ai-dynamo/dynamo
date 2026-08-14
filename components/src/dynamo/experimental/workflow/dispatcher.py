806d8497a48140e314bd80000ccf6453148e785b
furionw <qiwa@nvidia.com>
2026-08-13 21:59:53 -0700

    test(workflow): add three-process remote proof

--- components/src/dynamo/experimental/workflow/dispatcher.py
+++ components/src/dynamo/experimental/workflow/dispatcher.py
@@ -10,6 +10,7 @@ from types import MappingProxyType
 from typing import Any, Protocol, runtime_checkable
 
 from dynamo.experimental.workflow.plan import ExecutionPlan, InlineBinding, RemoteBinding
+from dynamo.experimental.workflow.remote import RemoteStageClient
 from dynamo.experimental.workflow.runtime import StageContext, StageRunner, WorkflowExecutionError
 from dynamo.experimental.workflow.types import WorkflowValidationError
 
@@ -105,8 +106,6 @@ class StageDispatcher:
         inline_runners: Mapping[str, StageRunner] = MappingProxyType({}),
     ) -> "StageDispatcher":
         """Resolve remote endpoints once and bind all physical stage targets."""
-
-        from dynamo.experimental.workflow.remote import RemoteStageClient
 
         endpoint_ids = {
             binding.endpoint_id
