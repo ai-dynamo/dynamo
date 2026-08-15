cfc9f135f41ff84bb14fba0bcd9b5969905ab25b
furionw <qiwa@nvidia.com>
2026-08-14 19:08:43 -0700

    refactor(workflow): bind frontend to orchestrator ABI

--- components/src/dynamo/workflow/__init__.py
+++ components/src/dynamo/workflow/__init__.py
@@ -5,11 +5,7 @@
 
 from dynamo.workflow.builder import StageHandle, Workflow
 from dynamo.workflow.compiler import DeploymentSpec, compile_workflow
-from dynamo.workflow.frontend import (
-    WorkflowFrontendApplication,
-    WorkflowTokenEngine,
-    load_workflow_frontend_application,
-)
+from dynamo.workflow.frontend import WorkflowTokenEngine, load_workflow_orchestrator
 from dynamo.workflow.ir import StageIR, WorkflowIR
 from dynamo.workflow.nixl import (
     NixlLeaseRegistry,
@@ -52,7 +48,6 @@ __all__ = [
     "WorkflowIR",
     "WorkflowExecutionError",
     "WorkflowOrchestrator",
-    "WorkflowFrontendApplication",
     "WorkflowTokenEngine",
     "WorkflowValidationError",
     "ExecutionPlan",
@@ -65,5 +60,5 @@ __all__ = [
     "RemoteStageClient",
     "RemoteStageServer",
     "compile_workflow",
-    "load_workflow_frontend_application",
+    "load_workflow_orchestrator",
 ]
