b22160231b690e78297613d2f677f6a5e9bd5923
furionw <qiwa@nvidia.com>
2026-08-25 13:34:40 -0700

    refactor(workflow): preserve experimental API boundary

--- components/src/dynamo/experimental/workflow/__init__.py
+++ components/src/dynamo/experimental/workflow/__init__.py
@@ -1,15 +1,23 @@
 # SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-"""Authoring, compilation, and execution for Dynamo inference workflows."""
+"""Experimental inference workflow APIs."""
 
 from dynamo.experimental.workflow.builder import StageHandle, Workflow
 from dynamo.experimental.workflow.compiler import DeploymentSpec, compile_workflow
 from dynamo.experimental.workflow.ir import StageIR, WorkflowIR
 from dynamo.experimental.workflow.orchestrator import WorkflowOrchestrator
 from dynamo.experimental.workflow.plan import ExecutionPlan, InlineBinding
-from dynamo.experimental.workflow.runtime import StageContext, StageRunner, WorkflowExecutionError
-from dynamo.experimental.workflow.types import StageContract, ValueRef, WorkflowValidationError
+from dynamo.experimental.workflow.runtime import (
+    StageContext,
+    StageRunner,
+    WorkflowExecutionError,
+)
+from dynamo.experimental.workflow.types import (
+    StageContract,
+    ValueRef,
+    WorkflowValidationError,
+)
 
 __all__ = [
     "DeploymentSpec",
