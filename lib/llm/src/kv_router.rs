98bee734f013d8ff519b38f792deb3ea54e20909
Chanh Nguyen <channguyen@nvidia.com>
2026-09-01 22:31:15 +0000

    perf(router): box sampled decision trace
    Signed-off-by: Chanh Nguyen <channguyen@nvidia.com>

--- lib/llm/src/kv_router.rs
+++ lib/llm/src/kv_router.rs
@@ -353,7 +353,7 @@ pub enum FindBestMatchOutcome {
         potential_decode_blocks: u64,
         routing_hashes: Option<RoutingDecisionHashes>,
         router_hint: Option<RouterHint>,
-        decision_trace: Option<dynamo_kv_router::protocols::RoutingDecisionTrace>,
+        decision_trace: Option<Box<dynamo_kv_router::protocols::RoutingDecisionTrace>>,
     },
     QueueRejected {
         rejection: scheduling::QueueRejection,
@@ -373,7 +373,7 @@ pub enum FindBestMatchAdvisoryOutcome {
         potential_decode_blocks: u64,
         selected_worker_load: scheduling::AdvisoryWorkerLoad,
         routing_hashes: Option<RoutingDecisionHashes>,
-        decision_trace: Option<dynamo_kv_router::protocols::RoutingDecisionTrace>,
+        decision_trace: Option<Box<dynamo_kv_router::protocols::RoutingDecisionTrace>>,
     },
     QueueRejected {
         rejection: scheduling::QueueRejection,
@@ -1715,7 +1715,7 @@ where
                         potential_decode_blocks: response.potential_decode_blocks as u64,
                         routing_hashes,
                         router_hint,
-                        decision_trace: response.decision_trace,
+                        decision_trace: response.decision_trace.map(Box::new),
                     },
                     attempt,
                 }),
@@ -1730,7 +1730,7 @@ where
                     selected_worker_load: selected_worker_load
                         .expect("without-admission selection returns advisory load"),
                     routing_hashes,
-                    decision_trace: response.decision_trace,
+                    decision_trace: response.decision_trace.map(Box::new),
                 }),
             ),
         }
