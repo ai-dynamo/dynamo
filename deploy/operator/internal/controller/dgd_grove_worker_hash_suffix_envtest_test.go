149c684251f0a5368f401843544683ce2a20a25c
Thomas Montfort <61255722+tmonty12@users.noreply.github.com>
2026-09-09 21:52:11 -0700

    fix(operator): gate worker hash projection on informer observation (part of #14222) (#14249)
    Signed-off-by: Thomas Montfort <tjmontfort12@gmail.com>
    Signed-off-by: Thomas Montfort <tmontfort@nvidia.com>

--- deploy/operator/internal/controller/dgd_grove_worker_hash_suffix_envtest_test.go
+++ deploy/operator/internal/controller/dgd_grove_worker_hash_suffix_envtest_test.go
@@ -165,6 +165,7 @@ func createLegacyGroveWorkerHashSuffixTestDGD(
 			},
 		}},
 	}
+	require.NoError(t, ctrl.SetControllerReference(dgd, legacyPCS, env.Client().Scheme()))
 	require.NoError(t, legacyClient.Create(ctx, legacyPCS))
 }
 
