fea57aa6c9512037b8576b3049672d043d72fc47
yunzhoul-nv <232973175+yunzhoul-nv@users.noreply.github.com>
2026-09-10 00:14:25 -0700

    chore: fix CI
    Signed-off-by: yunzhoul-nv <232973175+yunzhoul-nv@users.noreply.github.com>

--- scripts/validate-recipe-kustomization.py
+++ scripts/validate-recipe-kustomization.py
@@ -34,7 +34,8 @@ import os
 import re
 import subprocess
 import sys
-from dataclasses import dataclass, replace as dataclass_replace
+from dataclasses import dataclass
+from dataclasses import replace as dataclass_replace
 from pathlib import Path
 from typing import (
     Any,
