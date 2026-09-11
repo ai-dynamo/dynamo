591a9395669559a79c6ea73e337781b8fcb214bd
Alec Flowers <aflowers@nvidia.com>
2026-09-11 15:03:50 -0700

    build(vllm): centralize protocol dependency versions
    Signed-off-by: Alec Flowers <aflowers@nvidia.com>

--- lib/mocker/servers/vllm/src/server_tests.rs
+++ lib/mocker/servers/vllm/src/server_tests.rs
@@ -1,6 +1,9 @@
 // SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+use prost_types_v14 as prost_types;
+use tonic_v14 as tonic;
+
 use super::request::*;
 use super::*;
 use futures::StreamExt;
