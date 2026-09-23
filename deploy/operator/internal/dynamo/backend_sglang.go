d0a446d1ae52d38c9aecb8a460bf25663d0275e1
xianlubird <xianlubird@gmail.com>
2026-09-23 13:49:13 +0800

    fix(operator): reuse boolean constant for lint
    Signed-off-by: xianlubird <xianlubird@gmail.com>

--- deploy/operator/internal/dynamo/backend_sglang.go
+++ deploy/operator/internal/dynamo/backend_sglang.go
@@ -16,7 +16,8 @@ import (
 const (
 	SglangPort = "29500"
 
-	maxTCPPort = 65535
+	maxTCPPort       = 65535
+	booleanTrueValue = "true"
 
 	sglangEmbeddingWorkerFlag           = "--embedding-worker"
 	sglangNoEmbeddingWorkerFlag         = "--no-embedding-worker"
@@ -95,7 +96,7 @@ func sglangEmbeddingWorkerEnabled(container *corev1.Container) bool {
 	if env := findEnvVar(container.Env, sglangEmbeddingWorkerEnv); env != nil {
 		if env.ValueFrom == nil {
 			switch strings.ToLower(strings.TrimSpace(env.Value)) {
-			case "true", "1", "yes", "on":
+			case booleanTrueValue, "1", "yes", "on":
 				enabled = true
 			}
 		}
@@ -168,7 +169,7 @@ func reserveNixlExporterPorts(container *corev1.Container, containerGPUCount Con
 	if prometheusOn {
 		switch strings.ToLower(enabled.Value) {
 		case "y":
-		case "1", "yes", "on", "true", "enable":
+		case "1", "yes", "on", booleanTrueValue, "enable":
 			return nil
 		case "n", "0", "no", "off", "false", "disable":
 			return nil
